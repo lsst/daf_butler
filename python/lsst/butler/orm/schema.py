5995f3c6239dc5e36cdf4ac718a175f81f8900d1
Pim Schellart <P.Schellart@princeton.edu>
2017-10-16 12:18:15 -0400

    Add ORM schema for all no-join tables

---
+++ python/lsst/butler/orm/schema.py
@@ -0,0 +1,159 @@
+from sqlalchemy import Column, String, Integer, Boolean, LargeBinary, DateTime, ForeignKey, ForeignKeyConstraint
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Dataset(Base):
+    __tablename__ = 'Dataset'
+    dataset_id = Column(Integer, primary_key=True, nullable=False)
+    registry_id = Column(Integer, primary_key=True, nullable=False)
+    dataset_type_name = Column(Integer, ForeignKey('DatasetType.name'), nullable=False)
+    unit_pack = Column(LargeBinary, nullable=False)
+    uri = Column(String)
+    run_id = Column(Integer, nullable=False)
+    producer_id = Column(Integer)
+    ForeignKeyConstraint(['run_id', 'registry_id'], ['Run.run_id', 'Run.registry_id'])
+    ForeignKeyConstraint(['producer_id', 'registry_id'], ['Quantum.producer_id', 'Quantum.registry_id'])
+
+class DatasetComposition(Base):
+    __tablename__ = 'DatasetComposition'
+    parent_dataset_id = Column(Integer, primary_key=True, nullable=False)
+    parent_registry_id = Column(Integer, primary_key=True, nullable=False)
+    component_dataset_id = Column(Integer, primary_key=True, nullable=False)
+    component_registry_id = Column(Integer, primary_key=True, nullable=False)
+    component_name = Column(String, nullable=False)
+    ForeignKeyConstraint(['parent_dataset_id', 'parent_registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+    ForeignKeyConstraint(['component_dataset_id', 'component_registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+
+class DatasetType(Base):
+    __tablename__ = 'DatasetType'
+    name = Column(String, primary_key=True, nullable=False)
+    template = Column(String)
+    storage_class = Column(String, nullable=False)
+
+
+class DatasetTypeUnits(Base):
+    __tablename__ = 'DatasetTypeUnits'
+    dataset_type_name = Column(String, ForeignKey('DatasetType.name'), primary_key=True, nullable=False)
+    unit_name = Column(String, nullable=False)
+
+class DatasetCollections(Base):
+    __tablename__ = 'DatasetCollections'
+    tag = Column(String, nullable=False)
+    dataset_id = Column(Integer, primary_key=True, nullable=False)
+    registry_id = Column(Integer, primary_key=True, nullable=False)
+    ForeignKeyConstraint(['dataset_id', 'registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+
+class Run(Base):
+    __tablename__ = 'Run'
+    run_id = Column(Integer, primary_key=True, nullable=False)
+    registry_id = Column(Integer, primary_key=True, nullable=False)
+    tag	= Column(String)
+    environment_id = Column(Integer)
+    pipeline_id = Column(Integer)
+    ForeignKeyConstraint(['environment_id', 'registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+    ForeignKeyConstraint(['pipeline_id', 'registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+
+class Quantum(Base):
+    __tablename__ = 'Quantum'
+    quantum_id = Column(Integer, primary_key=True, nullable=False)
+    registry_id = Column(Integer, primary_key=True, nullable=False)
+    run_id = Column(Integer, nullable=False)
+    task = Column(String)
+    ForeignKeyConstraint(['run_id', 'registry_id'], ['Run.run_id', 'Run.registry_id'])
+
+class DatasetConsumers(Base):
+    __tablename__ = 'DatasetConsumers'
+    quantum_id = Column(Integer, ForeignKey('Quantum.quantum_id'), primary_key=True, nullable=False)
+    quantum_registry_id = Column(Integer, ForeignKey('Quantum.registry_id'), primary_key=True, nullable=False)
+    dataset_id = Column(Integer, ForeignKey('Dataset.dataset_id'), primary_key=True, nullable=False)
+    dataset_registry_id = Column(Integer, ForeignKey('Dataset.registry_id'), primary_key=True, nullable=False)
+    actual = Column(Boolean, nullable=False)
+    ForeignKeyConstraint(['quantum_id', 'quantum_registry_id'], ['Quantum.quantum_id', 'Quantum.registry_id'])
+    ForeignKeyConstraint(['dataset_id', 'dataset_registry_id'], ['Dataset.dataset_id', 'Dataset.registry_id'])
+
+class AbstractFilter(Base):
+    __tablename__ = 'AbstractFilter'
+    abstract_filter_name = Column(String, primary_key=True, nullable=False)
+
+class Camera(Base):
+    __tablename__ = 'Camera'
+    camera_name = Column(String, primary_key=True, nullable=False)
+    module = Column(String, primary_key=True, nullable=False)
+
+class PhysicalFilter(Base):
+    __tablename__ = 'PhysicalFilter'
+    physical_filter_name = Column(String, primary_key=True, nullable=False)
+    camera_name = Column(String, ForeignKey('Camera.camera_name'), primary_key=True, nullable=False)
+    abstract_filter_name = ForeignKey('AbstractFilter.abstract_filter_name'), Column(String)
+
+class PhysicalSensor(Base):
+    __tablename__ = 'PhysicalSensor'
+    physical_sensor_number = Column(String, primary_key=True, nullable=False)
+    name = Column(String, primary_key=True, nullable=False)
+    camera_name = Column(String, ForeignKey('Camera.camera_name'), nullable=False)
+    group = Column(String)
+    purpose = Column(String)
+
+class Visit(Base):
+    __tablename__ = 'Visit'
+    visit_number = Column(Integer, primary_key=True, nullable=False)
+    camera_name = Column(String, ForeignKey('Camera.camera_name'), primary_key=True, nullable=False)
+    physical_filter_name = Column(String, ForeignKey('PhysicalFilter.physical_filter_name'), nullable=False)
+    obs_begin = Column(DateTime)
+    obs_end = Column(DateTime)
+    region = Column(LargeBinary)
+
+class ObservedSensor(Base):
+    __tablename__ = 'ObservedSensor'
+    visit_number = Column(Integer, ForeignKey('Visit.visit_number'), primary_key=True, nullable=False)
+    physical_sensor_number = Column(Integer, ForeignKey('PhysicalSensor.physical_sensor_number'), primary_key=True, nullable=False)
+    camera_name	= Column(String, ForeignKey('Camera.camera_name'), primary_key=True, nullable=False)
+    region = Column(LargeBinary)
+
+class Snap(Base):
+    __tablename__ = 'Snap'
+    visit_number = Column(Integer, primary_key=True, nullable=False)
+    snap_index = Column(Integer, primary_key=True, nullable=False)
+    camera_name	= Column(String, ForeignKey('Camera.camera_name'), primary_key=True, nullable=False)
+    obs_begin = Column(DateTime, nullable=False)
+    obs_end	= Column(DateTime, nullable=False)
+    ForeignKeyConstraint(['visit_number', 'camera_name'], ['Visit.visit_number', 'Visit.camera_name'])
+
+class MasterCalib(Base):
+    __tablename__ = 'MasterCalib'
+    visit_begin = Column(Integer)
+    visit_end = Column(Integer)
+    physical_filter_name = Column(String, nullable=False)
+    camera_name	= Column(String, ForeignKey('Camera.camera_name'), primary_key=True, nullable=False)
+    ForeignKeyConstraint(['physical_filter_name', 'camera_name'], ['PhysicalFilter.physical_filter_name', 'PhysicalFilter.camera_name'])
+
+class SkyMap(Base):
+    __tablename__ = 'SkyMap'
+    skymap_name = Column(String, primary_key=True, nullable=False)
+    module = Column(String, nullable=False)
+    serialized = Column(LargeBinary, nullable=False)
+
+class Tract(Base):
+    __tablename__ = 'Tract'
+    tract_number = Column(Integer, primary_key=True, nullable=False)
+    skymap_name	= Column(String, ForeignKey('SkyMap.skymap_name'), primary_key=True, nullable=False)
+    region = Column(LargeBinary)
+
+class Patch(Base):
+    __tablename__ = 'Patch'
+    patch_index = Column(Integer, primary_key=True, nullable=False)
+    tract_number = Column(Integer, primary_key=True, nullable=False)
+    skymap_name = Column(String, ForeignKey('SkyMap.skymap_name'), primary_key=True, nullable=False)
+    region = Column(LargeBinary)
+    ForeignKeyConstraint(['tract_number', 'skymap_name'], ['Tract.tract_number', 'Tract.skymap_name'])
+
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:')
+
+from sqlalchemy.orm import sessionmaker
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.create_all(engine)
