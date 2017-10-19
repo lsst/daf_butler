4e805e139bfcc61f68b361a07cc2d0c95f12a24d
Pim Schellart <P.Schellart@princeton.edu>
2017-10-19 12:27:27 -0400

    Refactor tests, we need a Run in all of them

--- tests/test_orm.py
+++ tests/test_orm.py
@@ -31,44 +31,54 @@ class SchemaTestCase(unittest.TestCase):
     """A test case for the ORM schema."""
 
     def setUp(self):
+        self.registry_id = 0
+        self.run_id = 0
+        self.collection_tag = "default"
+        
+        self.createDatabase()
+        self.createRun(self.run_id, self.collection_tag)
+
+    def createDatabase(self):
         self.engine = create_engine('sqlite:///:memory:')
         self.sessionmaker = sessionmaker()
         self.sessionmaker.configure(bind=self.engine)
         Base.metadata.create_all(self.engine)
     
-    def testAddDataset(self):
+    def createRun(self, run_id, collection_tag):
         session = self.sessionmaker()
-        
+        run = Run(run_id=run_id, registry_id=self.registry_id, tag=collection_tag)
+        session.add(run)
+        session.commit()
+
+    def testRun(self):
+        session = self.sessionmaker()
+        runOut = session.query(Run).filter_by(run_id=self.run_id).first()
+        self.assertEquals(runOut.run_id, self.run_id)
+        self.assertEquals(runOut.tag, self.collection_tag)
+
+    def testAddDataset(self):
         dataset_id = 0
-        registry_id = 0
         dataset_type_name = "calexp"
         unit_pack = bytes()
         uri="http:://www.lsst.org/testdata/dataset-0"
-        run_id = 0
         producer_id = None
-
-        collection_tag = "first"
-        run = Run(run_id=run_id, registry_id=registry_id, tag=collection_tag)
-        session.add(run)
-
+        
+        session = self.sessionmaker()
+        
         storage_class = "Exposure"
         datasetType = DatasetType(name=dataset_type_name, storage_class=storage_class)
         session.add(datasetType)
 
         dataset = Dataset(dataset_id=dataset_id,
-                          registry_id=registry_id,
+                          registry_id=self.registry_id,
                           dataset_type_name=dataset_type_name,
                           unit_pack=unit_pack,
                           uri=uri,
-                          run_id=run_id,
+                          run_id=self.run_id,
                           producer_id=producer_id)
         session.add(dataset)
 
         session.commit()
-
-        runOut = session.query(Run).filter_by(run_id=run_id).first()
-        self.assertEquals(runOut.run_id, run_id)
-        self.assertEquals(runOut.tag, collection_tag)
 
         datasetTypeOut = session.query(DatasetType).filter_by(name=dataset_type_name).first()
         self.assertEquals(datasetTypeOut.name, dataset_type_name)
@@ -76,10 +86,10 @@ class SchemaTestCase(unittest.TestCase):
 
         datasetOut = session.query(Dataset).filter_by(dataset_id=dataset_id).first()
         self.assertEquals(datasetOut.dataset_id, dataset_id)
-        self.assertEquals(datasetOut.registry_id, registry_id)
+        self.assertEquals(datasetOut.registry_id, self.registry_id)
         self.assertEquals(datasetOut.dataset_type_name, dataset_type_name)
         self.assertEquals(datasetOut.unit_pack, unit_pack)
         self.assertEquals(datasetOut.uri, uri)
-        self.assertEquals(datasetOut.run_id, run_id)
+        self.assertEquals(datasetOut.run_id, self.run_id)
         self.assertEquals(datasetOut.producer_id, producer_id)
         
