4b15ebf1f98c5a3ee61aacc7011164fbc38ddb98
Jim Bosch <jbosch@astro.princeton.edu>
2017-10-21 12:23:52 -0400

    Finish initial ingest script for ci_hsc test data.

--- python/lsst/butler/ci_hsc/ingest.py
+++ python/lsst/butler/ci_hsc/ingest.py
@@ -7,35 +7,39 @@ import numpy as np
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from . import hsc
+from .. import units
 from ..orm.schema import Base
 
 
 CASTERS = {
     "i": int,
     "f": float,
-    "U": str,
+    "U": lambda x: str(x) if x else None,
     "S": bytes,
 }
 
 
-def getDatabaseName(root):
-    return os.path.join(root, "ci_hsc.sqlite3")
+DATA_ROOT = os.path.join(
+    os.path.split(__file__)[0],
+    "../../../../tests/data/ci_hsc"
+)
 
 
-def createTables(root):
-    engine = create_engine("sqlite:///{}".format(getDatabaseName(root)))
+def createTables(filename):
+    engine = create_engine("sqlite:///{}".format(filename))
     maker = sessionmaker()
     maker.configure(bind=engine)
     Base.metadata.create_all(engine)
     maker.close_all()
 
 
-def loadTable(db, root, name, placeholder="?", extra=None):
-    array = np.load(os.path.join(root, "{}.npy".format(name)))
+def loadTable(db, name, placeholder="?", extra=None):
+    array = np.load(os.path.join(DATA_ROOT, "{}.npy".format(name)))
     if extra is None:
         extra = {}
     keys = tuple(array.dtype.names)
-    fields = ", ".join(keys + tuple(extra.keys()))
+    fields = ", ".join('"%s"' % k for k in (keys + tuple(extra.keys())))
     placeholders = ", ".join([placeholder] * len(keys) + list(extra.values()))
     sql = "INSERT INTO {name} ({fields}) VALUES ({placeholders})".format(
         name=name, fields=fields, placeholders=placeholders
@@ -50,16 +54,58 @@ def loadTable(db, root, name, placeholder="?", extra=None):
     db.executemany(sql, (sanitize(r) for r in array))
 
 
-def run(root, create=True):
+def run(filename=None, create=True, skipCamera=False, verbose=False):
+    """Construct a SQLite database with Registry data for the ci_hsc test data.
+
+    The created database will have Dataset entries for raw, biad, dark, and flat,
+    associated with the appropriate DataUnits.
+
+    Parameters
+    ----------
+    filename: str
+        Name of the file that contiains / will contain the SQLite database.
+        Defaults to 'tests/data/ci_hsc/ci_hsc.sqlite3'.
+        The special ':memory:' database is not currently supported.'
+    create: bool
+        If True, delete the file if it exists and define the schema.  If False,
+        the database must exist and the full schema must already be present.
+    skipCamera: bool
+        If True, do not load the Camera, PhysicalFilter, or PhysicalSensor
+        tables, and instead assume they are already present in the database.
+    verbose: bool
+        If True, echo all database queries to stdout.
+    """
+    if not filename:
+        filename = os.path.join(DATA_ROOT, "ci_hsc.sqlite3")
     if create:
-        createTables(root)
-    db = sqlite3.connect(getDatabaseName(root))
+        if os.path.exists(filename):
+            os.remove(filename)
+        createTables(filename)
+    db = sqlite3.connect(filename)
+    if verbose:
+        db.set_trace_callback(print)
     db.execute("INSERT INTO Run (run_id, registry_id, tag) VALUES (0, 1, 'ingest')")
-    loadTable(db, root, "DatasetType")
-    loadTable(db, root, "DatasetTypeUnits")
-    loadTable(db, root, "Dataset", extra={"unit_pack": "''"})
+    loadTable(db, "DatasetType")
+    loadTable(db, "DatasetTypeUnits")
+    loadTable(db, "Dataset", extra={"unit_pack": "''"})
+    loadTable(db, "AbstractFilter")
+    if not skipCamera:
+        camera = units.Camera.instances["HSC"]
+        db.execute("INSERT INTO Camera (camera_name, module) VALUES (?, ?)",
+                   (camera.name, camera.__module__))
+        loadTable(db, "PhysicalFilter")
+        loadTable(db, "PhysicalSensor")
+    elif create:
+        raise RuntimeError("Cannot skip Camera definitions when creating a new database.")
+    loadTable(db, "Visit")
+    loadTable(db, "Snap")
+    loadTable(db, "ObservedSensor")
+    loadTable(db, "VisitDatasetJoin")
+    loadTable(db, "PhysicalSensorDatasetJoin")
+    loadTable(db, "VisitRange")
+    loadTable(db, "VisitRangeDatasetJoin")
+    loadTable(db, "PhysicalSensorDatasetJoin")
     db.commit()
 
 if __name__ == "__main__":
-    import sys
-    run(sys.argv[1])
+    run()
