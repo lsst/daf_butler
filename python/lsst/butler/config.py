306ff13647abb98a2b44a2e1df970af04c90b614
Pim Schellart <P.Schellart@princeton.edu>
2017-12-14 17:08:59 -0500

    Modify Policy to Config for use in the new Butler

--- python/lsst/butler/config.py
+++ python/lsst/butler/config.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-
 #
 # LSST Data Management System
-# Copyright 2015 LSST Corporation.
+#
+# Copyright 2008-2017  AURA/LSST.
 #
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -19,11 +18,8 @@
 #
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
-# see <http://www.lsstcorp.org/LegalNotices/>.
+# see <https://www.lsstcorp.org/LegalNotices/>.
 #
-from builtins import str
-from past.builtins import basestring
-from future.utils import with_metaclass
 
 import collections
 import copy
@@ -32,7 +28,6 @@ import sys
 import warnings
 import yaml
 
-import lsst.pex.policy as pexPolicy
 import lsst.utils
 
 from yaml.representer import Representer
@@ -40,39 +35,34 @@ yaml.add_representer(collections.defaultdict, Representer.represent_dict)
 
 # UserDict and yaml have defined metaclasses and Python 3 does not allow multiple
 # inheritance of classes with distinct metaclasses. We therefore have to
-# create a new baseclass that Policy can inherit from. This is because the metaclass
+# create a new baseclass that Config can inherit from. This is because the metaclass
 # syntax differs between versions
 
-if sys.version_info[0] >= 3:
-    class _PolicyMeta(type(collections.UserDict), type(yaml.YAMLObject)):
-        pass
-
-    class _PolicyBase(with_metaclass(_PolicyMeta, collections.UserDict, yaml.YAMLObject)):
-        pass
-else:
-    class _PolicyBase(collections.UserDict, yaml.YAMLObject):
-        pass
+class _ConfigMeta(type(collections.UserDict), type(yaml.YAMLObject)):
+    pass
 
 
-class Policy(_PolicyBase):
-    """Policy implements a datatype that is used by Butler for configuration parameters.
+class _ConfigBase(collections.UserDict, yaml.YAMLObject, metaclass=_ConfigMeta):
+    pass
+
+
+class Config(_ConfigBase):
+    """Config implements a datatype that is used by Butler for configuration parameters.
     It is essentially a dict with key/value pairs, including nested dicts (as values). In fact, it can be
     initialized with a dict. The only caveat is that keys may NOT contain dots ('.'). This is explained next:
-    Policy extends the dict api so that hierarchical values may be accessed with dot-delimited notiation.
+    Config extends the dict api so that hierarchical values may be accessed with dot-delimited notiation.
     That is, foo.getValue('a.b.c') is the same as foo['a']['b']['c'] is the same as foo['a.b.c'], and either
     of these syntaxes may be used.
 
     Storage formats supported:
     - yaml: read and write is supported.
-    - pex policy: read is supported, although this is deprecated and will at some point be removed.
     """
 
     def __init__(self, other=None):
-        """Initialize the Policy. Other can be used to initialize the Policy in a variety of ways:
-        other (string) Treated as a path to a policy file on disk. Must end with '.paf' or '.yaml'.
-        other (Pex Policy) Initializes this Policy with the values in the passed-in Pex Policy.
-        other (Policy) Copies the other Policy's values into this one.
-        other (dict) Copies the values from the dict into this Policy.
+        """Initialize the Config. Other can be used to initialize the Config in a variety of ways:
+        other (string) Treated as a path to a config file on disk. Must end with '.paf' or '.yaml'.
+        other (Config) Copies the other Config's values into this one.
+        other (dict) Copies the values from the dict into this Config.
         """
         collections.UserDict.__init__(self)
 
@@ -81,23 +71,20 @@ class Policy(_PolicyBase):
 
         if isinstance(other, collections.Mapping):
             self.update(other)
-        elif isinstance(other, Policy):
+        elif isinstance(other, Config):
             self.data = copy.deepcopy(other.data)
-        elif isinstance(other, basestring):
+        elif isinstance(other, str):
             # if other is a string, assume it is a file path.
             self.__initFromFile(other)
-        elif isinstance(other, pexPolicy.Policy):
-            # if other is an instance of a Pex Policy, load it accordingly.
-            self.__initFromPexPolicy(other)
         else:
-            # if the policy specified by other could not be loaded raise a runtime error.
-            raise RuntimeError("A Policy could not be loaded from other:%s" % other)
+            # if the config specified by other could not be loaded raise a runtime error.
+            raise RuntimeError("A Config could not be loaded from other:%s" % other)
 
     def ppprint(self):
-        """helper function for debugging, prints a policy out in a readable way in the debugger.
+        """helper function for debugging, prints a config out in a readable way in the debugger.
 
-        use: pdb> print myPolicyObject.pprint()
-        :return: a prettyprint formatted string representing the policy
+        use: pdb> print myConfigObject.pprint()
+        :return: a prettyprint formatted string representing the config
         """
         import pprint
         return pprint.pformat(self.data, indent=2, width=1)
@@ -109,40 +96,16 @@ class Policy(_PolicyBase):
         """Load a file from path. If path is a list, will pick one to use, according to order specified
         by extensionPreference.
 
-        :param path: string or list of strings, to a persisted policy file.
+        :param path: string or list of strings, to a persisted config file.
         :param extensionPreference: the order in which to try to open files. Will use the first one that
         succeeds.
         :return:
         """
-        policy = None
+        config = None
         if path.endswith('yaml'):
             self.__initFromYamlFile(path)
-        elif path.endswith('paf'):
-            policy = pexPolicy.Policy.createPolicy(path)
-            self.__initFromPexPolicy(policy)
         else:
-            raise RuntimeError("Unhandled policy file type:%s" % path)
-
-    def __initFromPexPolicy(self, pexPolicy):
-        """Load values from a pex policy.
-
-        :param pexPolicy:
-        :return:
-        """
-        names = pexPolicy.names()
-        names.sort()
-        for name in names:
-            if pexPolicy.getValueType(name) == pexPolicy.POLICY:
-                if name in self:
-                    continue
-                else:
-                    self[name] = {}
-            else:
-                if pexPolicy.isArray(name):
-                    self[name] = pexPolicy.getArray(name)
-                else:
-                    self[name] = pexPolicy.get(name)
-        return self
+            raise RuntimeError("Unhandled config file type:%s" % path)
 
     def __initFromYamlFile(self, path):
         """Opens a file at a given path and attempts to load it in from yaml.
@@ -154,7 +117,7 @@ class Policy(_PolicyBase):
             self.__initFromYaml(f)
 
     def __initFromYaml(self, stream):
-        """Loads a YAML policy from any readable stream that contains one.
+        """Loads a YAML config from any readable stream that contains one.
 
         :param stream:
         :return:
@@ -173,7 +136,7 @@ class Policy(_PolicyBase):
             else:
                 return None
         if isinstance(data, collections.Mapping):
-            data = Policy(data)
+            data = Config(data)
         return data
 
     def __setitem__(self, name, value):
@@ -203,15 +166,15 @@ class Policy(_PolicyBase):
         return keys[-1] in d
 
     @staticmethod
-    def defaultPolicyFile(productName, fileName, relativePath=None):
-        """Get the path to a default policy file.
+    def defaultConfigFile(productName, fileName, relativePath=None):
+        """Get the path to a default config file.
 
         Determines a directory for the product specified by productName. Then Concatenates
         productDir/relativePath/fileName (or productDir/fileName if relativePath is None) to find the path
-        to the default Policy file
+        to the default Config file
 
-        @param productName (string) The name of the product that the default policy is installed as part of
-        @param fileName (string) The name of the policy file. Can also include a path to the file relative to
+        @param productName (string) The name of the product that the default config is installed as part of
+        @param fileName (string) The name of the config file. Can also include a path to the file relative to
                                  the directory where the product is installed.
         @param relativePath (string) The relative path from the directior where the product is installed to
                                      the location where the file (or the path to the file) is found. If None
@@ -235,7 +198,7 @@ class Policy(_PolicyBase):
         foo.update({'a': {'c': 2}})
 
         If foo is a dict, then after the update foo == {'a': {'c': 2}}
-        But if foo is a Policy, then after the update foo == {'a': {'b': 1, 'c': 2}}
+        But if foo is a Config, then after the update foo == {'a': {'b': 1, 'c': 2}}
         """
         def doUpdate(d, u):
             for k, v in u.items():
@@ -251,7 +214,7 @@ class Policy(_PolicyBase):
         doUpdate(self.data, other)
 
     def merge(self, other):
-        """Like Policy.update, but will add keys & values from other that DO NOT EXIST in self. Keys and
+        """Like Config.update, but will add keys & values from other that DO NOT EXIST in self. Keys and
         values that already exist in self will NOT be overwritten.
 
         :param other:
@@ -287,119 +250,39 @@ class Policy(_PolicyBase):
         :return:
         """
         val = self.get(name)
-        if isinstance(val, basestring):
-            val = [val]
-        elif not isinstance(val, collections.Container):
-            val = [val]
-        return val
-
-    # Deprecated methods that mimic pex_policy api.
-    # These are supported (for now), but callers should use the dict api.
-
-    def getValue(self, name):
-        """Get the value for a parameter name/key. See class notes about dot-delimited access.
-
-        :param name:
-        :return: the value for the given name.
-        """
-        warnings.warn_explicit("Deprecated. Use []", DeprecationWarning)
-        return self[name]
-
-    def setValue(self, name, value):
-        """Set the value for a parameter name/key. See class notes about dot-delimited access.
-
-        :param name:
-        :return: None
-        """
-        warnings.warn("Deprecated. Use []", DeprecationWarning)
-        self[name] = value
-
-    def mergeDefaults(self, other):
-        """For any keys in other that are not present in self, sets that key and its value into self.
-
-        :param other: another Policy
-        :return: None
-        """
-        warnings.warn("Deprecated. Use .merge()", DeprecationWarning)
-        self.merge(other)
-
-    def exists(self, key):
-        """Query if a key exists in this Policy
-
-        :param key:
-        :return: True if the key exists, else false.
-        """
-        warnings.warn("Deprecated. Use 'key in object'", DeprecationWarning)
-        return key in self
-
-    def getString(self, key):
-        """Get the string value of a key.
-
-        :param key:
-        :return: the value for key
-        """
-        warnings.warn("Deprecated. Use []", DeprecationWarning)
-        return str(self[key])
-
-    def getBool(self, key):
-        """Get the value of a key.
-
-        :param key:
-        :return: the value for key
-        """
-        warnings.warn("Deprecated. Use []", DeprecationWarning)
-        return bool(self[key])
-
-    def getPolicy(self, key):
-        """Get a subpolicy.
-
-        :param key:
-        :return:
-        """
-        warnings.warn("Deprecated. Use []", DeprecationWarning)
-        return self[key]
-
-    def getStringArray(self, key):
-        """Get a value as an array. May contain one or more elements.
-
-        :param key:
-        :return:
-        """
-        warnings.warn("Deprecated. Use asArray()", DeprecationWarning)
-        val = self.get(key)
-        if isinstance(val, basestring):
+        if isinstance(val, str):
             val = [val]
         elif not isinstance(val, collections.Container):
             val = [val]
         return val
 
     def __lt__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data < other
 
     def __le__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data <= other
 
     def __eq__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data == other
 
     def __ne__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data != other
 
     def __gt__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data > other
 
     def __ge__(self, other):
-        if isinstance(other, Policy):
+        if isinstance(other, Config):
             other = other.data
         return self.data >= other
 
@@ -407,7 +290,7 @@ class Policy(_PolicyBase):
     # i/o #
 
     def dump(self, output):
-        """Writes the policy to a yaml stream.
+        """Writes the config to a yaml stream.
 
         :param stream:
         :return:
@@ -428,7 +311,7 @@ class Policy(_PolicyBase):
             yaml.safe_dump(data, output, default_flow_style=False)
 
     def dumpToFile(self, path):
-        """Writes the policy to a file.
+        """Writes the config to a file.
 
         :param path:
         :return:
