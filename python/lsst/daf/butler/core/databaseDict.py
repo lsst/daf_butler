b8c1459bfe338979be6e3ac994c96a1aeae2043c
Jim Bosch <jbosch@astro.princeton.edu>
2018-04-26 17:37:29 -0400

    Add database-backed dict hierarchy.
    Primarily intended for Datastore's internal records.

---
+++ python/lsst/daf/butler/core/databaseDict.py
@@ -0,0 +1,105 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections.abc import MutableMapping
+
+from .utils import doImport
+
+
+class DatabaseDict(MutableMapping):
+    """An abstract base class for dict-like objects with a specific key type
+    and namedtuple values, backed by a database.
+
+    DatabaseDict subclasses must implement the abstract ``__getitem__``,
+    ``__setitem__``, ``__delitem__`, ``__iter__``, and ``__len__`` abstract
+    methods defined by `MutableMapping`.
+
+    They must also provide a constructor that takes the same arguments as that
+    of `DatabaseDict` itself, *unless* they are constructed solely by
+    `Registry.makeDatabaseDict` (in which case any constructor arguments are
+    permitted).
+
+    Parameters
+    ----------
+    config : `Config`
+        Configuration used to identify and construct a subclass.
+    types : `dict`
+        A dictionary mapping `str` field names to type objects, containing
+        all fields to be held in the database.
+    key : `str`
+        The name of the field to be used as the dictionary key.  Must not be
+        present in ``value._fields``.
+    value : `type`
+        The type used for the dictionary's values, typically a `namedtuple`.
+        Must have a ``_fields`` class attribute that is a tuple of field names
+        (i.e. as defined by `namedtuple`); these field names must also appear
+        in the ``types`` arg, and a `_make` attribute to construct it from a
+        sequence of values (again, as defined by `namedtuple`).
+    """
+
+    @staticmethod
+    def fromConfig(config, types, key, value, registry=None):
+        """Create a `DatabaseDict` subclass instance from `config`.
+
+        If ``config`` contains a class ``cls`` key, this will be assumed to
+        be the fully-qualified name of a DatabaseDict subclass to construct.
+        If not, ``registry.makeDatabaseDict`` will be called instead.
+
+        Parameters
+        ----------
+        config : `Config`
+            Configuration used to identify and construct a subclass.
+        types : `dict`
+            A dictionary mapping `str` field names to type objects, containing
+            all fields to be held in the database.
+        key : `str`
+            The name of the field to be used as the dictionary key.  Must not
+            be present in ``value._fields``.
+        value : `type`
+            The type used for the dictionary's values, typically a
+            `namedtuple`. Must have a ``_fields`` class attribute that is a
+            tuple of field names (i.e. as defined by `namedtuple`); these
+            field names must also appear in the ``types`` arg, and a `_make`
+            attribute to construct it from a sequence of values (again, as
+            defined by `namedtuple`).
+        registry : `Registry`
+            A registry instance from which a `DatabaseDict` subclass can be
+            obtained.  Ignored if ``config["cls"]`` exists; may be None if
+            it does.
+
+        Returns
+        -------
+        dictionary : `DatabaseDict` (subclass)
+            A new `DatabaseDict` subclass instance.
+        """
+        if 'cls' in config:
+            cls = doImport(config['cls'])
+            return cls(config=config, types=types, key=key, value=value)
+        else:
+            table = config['table']
+            if registry is None:
+                raise ValueError("Either config['cls'] or registry must be provided.")
+            return registry.makeDatabaseDict(table, types=types, key=key, value=value)
+
+    def __init__(self, config, types, key, value):
+        # This constructor is currently defined just to clearly document the
+        # interface subclasses should conform to.
+        pass
---
+++ tests/test_sqlDatabaseDict.py
@@ -0,0 +1,143 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+from collections import namedtuple
+
+import lsst.utils.tests
+
+from lsst.daf.butler.core import Config, DatabaseDict, Registry
+
+"""Tests for SqlDatabaseDict.
+"""
+
+
+class SqlDatabaseDictTestCase(lsst.utils.tests.TestCase):
+    """Test for SqlDatabaseDict.
+    """
+
+    def setUp(self):
+        self.config = Config()
+        self.config["cls"] = "lsst.daf.butler.core.sqlDatabaseDict.SqlDatabaseDict"
+        self.config["db"] = "sqlite:///:memory:"
+        self.config["table"] = "TestTable"
+        self.types = {"x": int, "y": str, "z": float}
+        self.key = "x"
+
+    def checkDatabaseDict(self, d, data):
+        self.assertEqual(len(d), 0)
+        self.assertFalse(d)
+        d[0] = data[0]
+        self.assertEqual(len(d), 1)
+        self.assertTrue(d)
+        self.assertIn(0, d)
+        self.assertEqual(d[0], data[0])
+        d[1] = data[1]
+        self.assertEqual(len(d), 2)
+        self.assertTrue(d)
+        self.assertIn(1, d)
+        self.assertEqual(d[1], data[1])
+        self.assertCountEqual(d.keys(), data.keys())
+        self.assertCountEqual(d.values(), data.values())
+        self.assertCountEqual(d.items(), data.items())
+        del d[0]
+        self.assertNotIn(0, d)
+        self.assertEqual(len(d), 1)
+        with self.assertRaises(KeyError):
+            del d[0]
+        with self.assertRaises(KeyError):
+            d[0]
+        # Test that we can update an existing key
+        d[1] = data[0]
+        self.assertEqual(len(d), 1)
+        self.assertEqual(d[1], data[0])
+
+    def testKeyInValue(self):
+        """Test that the key is not permitted to be part of the value."""
+        value = namedtuple("TestValue", ["x", "y", "z"])
+        with self.assertRaises(ValueError):
+            DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+
+    def testKeyNotInValue(self):
+        """Test when the value does not include the key."""
+        value = namedtuple("TestValue", ["y", "z"])
+        data = {
+            0: value(y="zero", z=0.0),
+            1: value(y="one", z=0.1),
+        }
+        d = DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+        self.checkDatabaseDict(d, data)
+
+    def testBadValueTypes(self):
+        """Test that we cannot insert value tuples with the wrong types."""
+        value = namedtuple("TestValue", ["y", "z"])
+        data = {
+            0: value(y=0, z="zero"),
+        }
+        d = DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+        with self.assertRaises(TypeError):
+            d[0] = data[0]
+
+    def testBadKeyTypes(self):
+        """Test that we cannot insert with the wrong key type."""
+        value = namedtuple("TestValue", ["y", "z"])
+        data = {
+            0: value(y="zero", z=0.0),
+        }
+        d = DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+        d["zero"] = data[0]
+
+    def testExtraFieldsInTable(self):
+        """Test when there are fields in the table that not in the value or the key.
+
+        These should be completely ignored by the DatabaseDict after the table
+        is created (which implies that they must be nullable if __setitem__ is
+        expected to work."""
+        value = namedtuple("TestValue", ["y"])
+        data = {
+            0: value(y="zero"),
+            1: value(y="one"),
+        }
+        d = DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+        self.checkDatabaseDict(d, data)
+
+    def testExtraFieldsInValue(self):
+        """Test that we don't permit the value tuple to have ._fields entries
+        that are not in the types argument itself (since we need to know
+        their types).
+        """
+        value = namedtuple("TestValue", ["y", "a"])
+        with self.assertRaises(TypeError):
+            DatabaseDict.fromConfig(self.config, key=self.key, types=self.types, value=value)
+
+
+class MemoryTester(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
