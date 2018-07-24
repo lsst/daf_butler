c91bb10fd2f8210a81a2594518d0a834a0d6e2bf
Tim Jenness <tjenness@lsst.org>
2018-07-24 10:33:13 -0700

    Add function for calculating size of a python object

--- python/lsst/daf/butler/core/utils.py
+++ python/lsst/daf/butler/core/utils.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 import functools
 import importlib
 from collections import namedtuple
@@ -323,3 +324,46 @@ def transactional(func):
         with self.transaction():
             return func(self, *args, **kwargs)
     return inner
+
+
+def get_object_size(obj, seen=None):
+    """Recursively finds size of objects.
+
+    Parameters
+    ----------
+    obj : `object`
+       Instance for which size is to be calculated.
+    seen : `set`, optional
+       Used internally to keep track of objects already sized during
+       recursion.
+
+    Returns
+    -------
+    size : `int`
+       Size in bytes.
+
+    See Also
+    --------
+    sys.getsizeof
+
+    Notes
+    -----
+    See https://goshippo.com/blog/measure-real-size-any-python-object/
+    """
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_object_size(v, seen) for v in obj.values()])
+        size += sum([get_object_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_object_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_object_size(i, seen) for i in obj])
+    return size
