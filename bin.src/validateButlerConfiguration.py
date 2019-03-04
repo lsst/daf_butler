bb8ea53b37fdd7ca6046182fa8b87a157e189894
Tim Jenness <tjenness@lsst.org>
2019-03-04 15:37:15 -0700

    Add command line tool to validate a butler configuration

---
+++ bin.src/validateButlerConfiguration.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+
+from lsst.daf.butler import Butler, ValidationError
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Validate the configuration files for a "
+                                     "Gen3 Butler repository.")
+    parser.add_argument("root",
+                        help="Filesystem path for an existing Butler repository.")
+    parser.add_argument("--collection", "-c", default="validate", type=str,
+                        help="Collection to refer to in this repository.")
+
+    args = parser.parse_args()
+    # The collection does not matter for validation but if a run is specified
+    # in the configuration then it must be consistent with this collection
+    butler = Butler(config=args.root, collection=args.collection)
+    try:
+        butler.validateConfiguration(logFailures=True)
+    except ValidationError:
+        pass
+    else:
+        print("No problems encountered with configuration.")
