ce5f0d1181cc25fa075d898aa89ad28d647012de
Nate Lust <nlust@astro.princeton.edu>
2019-04-01 20:30:49 -0500

    Remove need for explicit close method
    Nullpools directly give control of connection objects to the code
    which calls the connection object. These connections are then closed
    when they are returned to the pool. However, like all pools the
    connections are wrapped in finalize_fairy methods such that when the
    pool is closed this function attempts to clean up resources. Because
    the connections is closed at the time it is returned to the pool this
    cleanup attempts to use a connection that is already closed, which
    throws an exception.
    
    Instead for oracle connections the butler now uses the standard
    QueuePool, limited to one connection. This allows the connections to
    properly be handled and cleaned up by the pool.
    
    Because the connections no longer need closed, there is no longer a
    need for additional close methods in the butler and registry. These
    methods are deleted in this commit.

--- python/lsst/daf/butler/registries/oracleRegistry.py
+++ python/lsst/daf/butler/registries/oracleRegistry.py
@@ -25,7 +25,6 @@ import re
 
 from sqlalchemy import event
 from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
 
 
 from lsst.daf.butler.core.config import Config
@@ -108,6 +107,6 @@ class OracleRegistry(SqlRegistry):
                     # single quote, optional close double quote
                     statement = re.sub(f"\"?'?{name}\\b'?\"?", lambda x: _ignoreQuote(name, x), statement)
             return statement, parameters
-        engine = create_engine(self.config["db"], poolclass=NullPool)
+        engine = create_engine(self.config["db"], pool_size=1)
         event.listen(engine, "before_cursor_execute", _oracleExecute, retval=True)
         return engine
