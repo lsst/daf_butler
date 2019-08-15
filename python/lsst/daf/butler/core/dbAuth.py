f0d7ed509a69bf679ea9d48977dadcfa1cf514b4
Kian-Tat Lim <ktl@slac.stanford.edu>
2019-08-15 11:14:27 -0700

    Retrieve DB authentication data.

---
+++ python/lsst/daf/butler/core/dbAuth.py
@@ -0,0 +1,227 @@
+# This file is part of daf_butler
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import fnmatch
+import os
+import re
+import stat
+import urllib.parse
+import yaml
+
+__all__ = ["DbAuth", "DbAuthError"]
+
+
+class DbAuthError(RuntimeError):
+    """A problem has occurred retrieving database authentication information.
+    """
+    pass
+
+
+class DbAuth:
+    """Retrieves authentication information for database connections.
+
+    The authorization configuration is taken from the ``authList`` parameter
+    or a (group- and world-inaccessible) YAML file located at a path specified
+    by the given environment variable or at a default path location.
+
+    Parameters
+    ----------
+    path : `str` or None, optional
+        Path to configuration file.
+    envVar : `str` or None, optional
+        Name of environment variable pointing to configuration file.
+    authList : `list` [`dict`] or None, optional
+        Authentication configuration.
+
+    Notes
+    -----
+    At least one of ``path``, ``envVar``, or ``authList`` must be provided;
+    generally ``path`` should be provided as a default location.
+    """
+    def __init__(self, path=None, envVar=None, authList=None):
+        if authList is not None:
+            self.authList = authList
+            return
+        if envVar is not None and envVar in os.environ:
+            secretPath = os.path.expanduser(os.environ[envVar])
+        elif path is None:
+            raise DbAuthError(
+                "No default path provided to DbAuth configuration file")
+        else:
+            secretPath = os.path.expanduser(path)
+        if not os.path.isfile(secretPath):
+            raise DbAuthError("No DbAuth configuration file: " + secretPath)
+        mode = os.stat(secretPath).st_mode
+        if mode & (stat.S_IRWXG | stat.S_IRWXO) != 0:
+            raise DbAuthError(
+                "DbAuth configuration file {} has incorrect permissions: "
+                "{:o}".format(secretPath, mode))
+
+        try:
+            with open(secretPath) as secretFile:
+                self.authList = yaml.safe_load(secretFile)
+        except Exception as exc:
+            raise DbAuthError(
+                "Unable to load DbAuth configuration file: " +
+                secretPath) from exc
+
+    def getAuth(self, drivername, username, host, port, database):
+        """Retrieve a username and password for a database connection.
+
+        This function matches elements from the database connection URL with
+        glob-like URL patterns in a list of configuration dictionaries.
+
+        Parameters
+        ----------
+        drivername : `str`
+            Name of database backend driver from connection URL.
+        username : `str` or None
+            Username from connection URL if present.
+        host : `str`
+            Host name from connection URL if present.
+        port : `str` or None
+            Port from connection URL if present.
+        database : `str`
+            Database name from connection URL.
+
+        Returns
+        -------
+        username: `str`
+            Username to use for database connection; same as parameter if
+            present.
+        password: `str`
+            Password to use for database connection.
+
+        Raises
+        ------
+        DbAuthError
+            Raised if the input is missing elements, an authorization
+            dictionary is missing elements, the authorization file is
+            misconfigured, or no matching authorization is found.
+
+        Notes
+        -----
+        The list of authorization configuration dictionaries is tested in
+        order, with the first matching dictionary used.  Each dictionary must
+        contain a ``url`` item with a pattern to match against the database
+        connection URL and a ``password`` item.  If no username is provided in
+        the database connection URL, the dictionary must also contain a
+        ``username`` item.
+
+        Glob-style patterns (using "*" and "?" as wildcards) can be used to
+        match the host and database name portions of the connection URL.  For
+        the username, port, and database name portions, omitting them from the
+        pattern matches against any value in the connection URL.
+
+        Examples
+        --------
+
+        The connection URL
+        "postgresql://user@host.example.com:5432/my_database" matches against
+        the identical string as a pattern.  Other patterns that would match
+        include:
+
+        * "postgresql://*"
+        * "postgresql://*.example.com"
+        * "postgresql://*.example.com/my_*"
+        * "postgresql://host.example.com/my_database"
+        * "postgresql://host.example.com:5432/my_database"
+        * "postgresql://user@host.example.com/my_database"
+
+        Note that the connection URL
+        "postgresql://host.example.com/my_database" would not match against
+        the pattern "postgresql://host.example.com:5432", even if the default
+        port for the connection is 5432.
+        """
+        if drivername is None or drivername == "":
+            raise DbAuthError("Missing drivername parameter")
+        if host is None or host == "":
+            raise DbAuthError("Missing host parameter")
+        if database is None or database == "":
+            raise DbAuthError("Missing database parameter")
+
+        for authDict in self.authList:
+
+            # Check for mandatory entries
+            if "url" not in authDict:
+                raise DbAuthError("Missing URL in DbAuth configuration")
+            if "password" not in authDict:
+                raise DbAuthError(
+                    "Missing password in DbAuth configuration "
+                    "for URL: " + authDict["url"])
+
+            # Parse pseudo-URL from db-auth.yaml
+            components = urllib.parse.urlparse(authDict["url"])
+
+            # Check for same database backend type/driver
+            if components.scheme == "":
+                raise DbAuthError(
+                    "Missing database driver in URL: " + authDict["url"])
+            if drivername != components.scheme:
+                continue
+
+            # Check for same database name
+            if components.path != "" and components.path != "/":
+                if not fnmatch.fnmatch(database, components.path.lstrip("/")):
+                    continue
+
+            # Parse out user if present and check
+            if components.netloc == "":
+                raise DbAuthError("Missing host in URL: " + authDict["url"])
+            if "@" in components.netloc:
+                matchUser, matchHost = components.netloc.split("@")
+                if (username is None or username == "") and matchUser != "":
+                    continue
+                if username is not None and username != matchUser:
+                    continue
+            else:
+                matchHost = components.netloc
+
+            # Parse out host and port and check
+            matchPort = None
+            # Check for IP v6
+            if matchHost.startswith("["):
+                match = re.search(r"^(\[[0-9a-f:]+\]):(\d+)$", matchHost)
+                if match:
+                    matchHost, matchPort = match.groups()
+                # Prep for fnmatch
+                matchHost = re.sub(r"^\[", "[[]", matchHost)
+            elif ":" in matchHost:
+                matchHost, matchPort = matchHost.split(":")
+            if matchPort is not None and (port is None or port != matchPort):
+                continue
+            if not fnmatch.fnmatch(host, matchHost):
+                continue
+
+            # Don't override username from connection string
+            if username is not None and username != "":
+                return (username, authDict["password"])
+            else:
+                if "username" not in authDict:
+                    raise DbAuthError(
+                        "Missing username in DbAuth configuration for "
+                        "URL: " + authDict["url"])
+                return (authDict["username"], authDict["password"])
+
+        raise DbAuthError(
+            "No matching DbAuth configuration for: "
+            "({}, {}, {}, {}, {})".format(
+                drivername, username, host, port, database))
---
+++ tests/test_dbAuth.py
@@ -0,0 +1,278 @@
+# This file is part of daf_butler
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+
+from lsst.daf.butler import DbAuth, DbAuthError
+
+TESTDIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "data", "dbAuth")
+
+
+class DbAuthTestCase(unittest.TestCase):
+    """Test DbAuth class."""
+
+    def test_patterns(self):
+        for matchPattern in [
+                "postgresql://*",
+                "postgresql://*.example.com",
+                "postgresql://*.example.com/my_*",
+                "postgresql://host.example.com/my_database",
+                "postgresql://host.example.com:5432/my_database",
+                "postgresql://user@host.example.com/my_database"]:
+            auth = DbAuth(authList=[
+                dict(url=matchPattern, username="foo",
+                     password="bar")])
+            user, pwd = auth.getAuth(
+                "postgresql", "user", "host.example.com", "5432",
+                "my_database")
+            self.assertEqual(user, "user")
+            self.assertEqual(pwd, "bar")
+
+    def test_connStrings(self):
+        auth = DbAuth(authList=[
+            dict(url="postgresql://host.example.com/my_database",
+                 username="foo", password="bar")])
+        for connComponents in [
+                ("postgresql", "user", "host.example.com", "5432",
+                    "my_database"),
+                ("postgresql", "user", "host.example.com", None,
+                    "my_database")]:
+            user, pwd = auth.getAuth(*connComponents)
+            self.assertEqual(user, "user")
+            self.assertEqual(pwd, "bar")
+        for connComponents in [
+                ("postgresql", None, "host.example.com", None,
+                    "my_database"),
+                ("postgresql", None, "host.example.com", "5432",
+                    "my_database")]:
+            user, pwd = auth.getAuth(*connComponents)
+            self.assertEqual(user, "foo")
+            self.assertEqual(pwd, "bar")
+
+    def test_load(self):
+        filePath = os.path.join(TESTDIR, "good_test.yaml")
+        os.chmod(filePath, 0o600)
+        auth = DbAuth(filePath)
+        self.assertEqual(
+            auth.getAuth("postgresql", "user", "host.example.com",
+                         "5432", "my_database"),
+            ("user", "test1"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user", "host.example.com",
+                         "3360", "my_database"),
+            ("user", "test2"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "", "host.example.com",
+                         "5432", "my_database"),
+            ("user", "test3"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host.example.com",
+                         "", "my_database"),
+            ("user", "test4"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "", "host2.example.com",
+                         None, "my_other"),
+            ("user", "test5"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host3.example.com",
+                         "3360", "some_database"),
+            ("user", "test6"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user", "host4.other.com",
+                         None, "other_database"),
+            ("user", "test8"))
+
+    def test_ipv6(self):
+        auth = DbAuth(authList=[
+            dict(url="postgresql://user@[fe80::1ff:fe23:4567:890a]:5432/db",
+                 password="pwd"),
+            dict(url="postgresql://[fe80::1ff:fe23:4567:890a]:5432/db",
+                 password="pwd2"),
+            dict(url="postgresql://user3@[fe80::1ff:fe23:4567:890a]/db",
+                 password="pwd3"),
+            dict(url="postgresql://[fe80::1ff:fe23:4567:890a]/db",
+                 password="pwd4")])
+        self.assertEqual(
+            auth.getAuth("postgresql", "user",
+                         "[fe80::1ff:fe23:4567:890a]", "5432", "db"),
+            ("user", "pwd"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user2",
+                         "[fe80::1ff:fe23:4567:890a]", "5432", "db"),
+            ("user2", "pwd2"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user3",
+                         "[fe80::1ff:fe23:4567:890a]", "3360", "db"),
+            ("user3", "pwd3"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user4",
+                         "[fe80::1ff:fe23:4567:890a]", "3360", "db"),
+            ("user4", "pwd4"))
+
+    def test_search(self):
+        auth = DbAuth(authList=[
+            dict(url="postgresql://user1@host2.example.com:3360/database3",
+                 password="first"),
+            dict(url="postgresql://host2.example.com:3360/database3",
+                 username="second_u", password="second"),
+            dict(url="postgresql://host2.example.com:5432/database3",
+                 username="wrong", password="port"),
+            dict(url="postgresql://host3.example.com/",
+                 username="third_u", password="third"),
+            dict(url="oracle://oracle.example.com/other_database",
+                 username="scott", password="tiger"),
+            dict(url="postgresql://*.example.com/database3",
+                 username="fourth_u", password="fourth"),
+            dict(url="postgresql://*.example.com",
+                 username="fifth_u", password="fifth")])
+        self.assertEqual(
+            auth.getAuth("postgresql", "user1", "host2.example.com",
+                         "3360", "database3"),
+            ("user1", "first"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "user2", "host2.example.com",
+                         "3360", "database3"),
+            ("user2", "second"))
+        self.assertEqual(
+            auth.getAuth("postgresql", "", "host2.example.com",
+                         "3360", "database3"),
+            ("second_u", "second"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host2.example.com",
+                         "3360", "database3"),
+            ("second_u", "second"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host3.example.com",
+                         "3360", "database3"),
+            ("third_u", "third"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host3.example.com",
+                         "3360", "database4"),
+            ("third_u", "third"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host4.example.com",
+                         "3360", "database3"),
+            ("fourth_u", "fourth"))
+        self.assertEqual(
+            auth.getAuth("postgresql", None, "host4.example.com",
+                         "3360", "database4"),
+            ("fifth_u", "fifth"))
+
+    def test_errors(self):
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^No default path provided to DbAuth configuration file$"):
+            auth = DbAuth()
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^No DbAuth configuration file: .*this_does_not_exist$"):
+            auth = DbAuth(os.path.join(TESTDIR, "this_does_not_exist"))
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^No DbAuth configuration file: .*this_does_not_exist$"):
+            auth = DbAuth(os.path.join(TESTDIR, "this_does_not_exist"),
+                          envVar="LSST_NONEXISTENT")
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^DbAuth configuration file .*bad_test1.yaml has incorrect "
+                r"permissions: \d*644$"):
+            filePath = os.path.join(TESTDIR, "bad_test1.yaml")
+            os.chmod(filePath, 0o644)
+            auth = DbAuth(filePath)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Unable to load DbAuth configuration file: "
+                r".*bad_test2.yaml"):
+            filePath = os.path.join(TESTDIR, "bad_test2.yaml")
+            os.chmod(filePath, 0o600)
+            os.environ["LSST_DB_AUTH_TEST"] = filePath
+            auth = DbAuth(envVar="LSST_DB_AUTH_TEST")
+
+        auth = DbAuth(authList=[])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing drivername parameter$"):
+            auth.getAuth(None, None, None, None, None)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing drivername parameter$"):
+            auth.getAuth("", None, None, None, None)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing host parameter$"):
+            auth.getAuth("postgresql", None, None, None, None)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing host parameter$"):
+            auth.getAuth("postgresql", None, "", None, None)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing database parameter$"):
+            auth.getAuth("postgresql", None, "example.com", None, None)
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing database parameter$"):
+            auth.getAuth("postgresql", None, "example.com", None, "")
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^No matching DbAuth configuration for: "
+                r"\(postgresql, None, example\.com, None, foo\)$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+        auth = DbAuth(authList=[dict(url="postgresql://*.example.com")])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing password in DbAuth configuration for URL: "
+                r"postgresql://\*\.example\.com$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+        auth = DbAuth(authList=[dict(password="testing")])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing URL in DbAuth configuration$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+        auth = DbAuth(authList=[dict(url="testing", password="testing")])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing database driver in URL: testing$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+        auth = DbAuth(authList=[
+            dict(url="postgresql:///foo", password="testing")])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing host in URL: postgresql:///foo$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+        auth = DbAuth(authList=[
+            dict(url="postgresql://example.com/foo", password="testing")])
+        with self.assertRaisesRegex(
+                DbAuthError,
+                r"^Missing username in DbAuth configuration for URL: "
+                r"postgresql://example.com/foo$"):
+            auth.getAuth("postgresql", None, "example.com", None, "foo")
+
+
+if __name__ == "__main__":
+    unittest.main()
