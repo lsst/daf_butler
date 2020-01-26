e25ff610b82781633531557998c7d410efe0b31b
Jim Bosch <jbosch@astro.princeton.edu>
2020-01-26 13:18:03 -0500

    Make most Butler attributes private, document the rest.
    The registry attribute definitely needs to be public.  The datastore
    might for particular datastore implementations (though maybe not any of
    the ones we have now).  Other attributes are definitely implementation
    details - I was actually thinking about removing config, since we don't
    *need* it after construction, and while that looks inconvenient now,
    we should leave the possibility open.
    
    This means we have a bit of test code using a private attribute
    (._config), but I think a bit of fragility there is better than
    removing those tests.

--- python/lsst/daf/butler/_butler.py
+++ python/lsst/daf/butler/_butler.py
@@ -134,21 +134,21 @@ class Butler:
                 raise TypeError("Cannot pass config or searchPaths arguments with butler argument.")
             self.registry = butler.registry
             self.datastore = butler.datastore
-            self.storageClasses = butler.storageClasses
-            self.composites = butler.composites
-            self.config = butler.config
+            self._storageClasses = butler.storageClasses
+            self._composites = butler.composites
+            self._config = butler.config
         else:
-            self.config = ButlerConfig(config, searchPaths=searchPaths)
-            if "root" in self.config:
-                butlerRoot = self.config["root"]
+            self._config = ButlerConfig(config, searchPaths=searchPaths)
+            if "root" in self._config:
+                butlerRoot = self._config["root"]
             else:
-                butlerRoot = self.config.configDir
-            self.registry = Registry.fromConfig(self.config, butlerRoot=butlerRoot)
-            self.datastore = Datastore.fromConfig(self.config, self.registry, butlerRoot=butlerRoot)
-            self.storageClasses = StorageClassFactory()
-            self.storageClasses.addFromConfig(self.config)
-            self.composites = CompositesMap(self.config, universe=self.registry.dimensions)
-        if "run" in self.config or "collection" in self.config:
+                butlerRoot = self._config.configDir
+            self.registry = Registry.fromConfig(self._config, butlerRoot=butlerRoot)
+            self.datastore = Datastore.fromConfig(self._config, self.registry, butlerRoot=butlerRoot)
+            self._storageClasses = StorageClassFactory()
+            self._storageClasses.addFromConfig(self._config)
+            self._composites = CompositesMap(self._config, universe=self.registry.dimensions)
+        if "run" in self._config or "collection" in self._config:
             raise ValueError("Passing a run or collection via configuration is no longer supported.")
         self.run = run
         # if run is not None and collection arg is, use run for collection.
@@ -170,6 +170,21 @@ class Butler:
     This attribute may be removed in the future, once the Generation 2 Butler
     interface has been fully retired; it should only be used in transitional
     code.
+    """
+
+    registry: Registry
+    """The object that manages dataset metadata and relationships (`Registry`).
+
+    Most operations that don't involve reading or writing butler datasets are
+    accessible only via `Registry` methods.
+    """
+
+    datastore: Datastore
+    """The object that manages actual dataset storage (`Datastore`).
+
+    Direct user access to the datastore should rarely be necessary; the primary
+    exception is the case where a `Datastore` implementation provides extra
+    functionality beyond what the base class defines.
     """
 
     @staticmethod
@@ -317,7 +332,7 @@ class Butler:
     def __reduce__(self):
         """Support pickling.
         """
-        return (Butler._unpickle, (self.config, self.collection, self.run))
+        return (Butler._unpickle, (self._config, self.collection, self.run))
 
     def __str__(self):
         return "Butler(collection='{}', datastore='{}', registry='{}')".format(
@@ -429,7 +444,7 @@ class Butler:
         if isinstance(datasetRefOrType, DatasetRef) and datasetRefOrType.id is not None:
             raise ValueError("DatasetRef must not be in registry, must have None id")
 
-        isVirtualComposite = self.composites.shouldBeDisassembled(datasetType)
+        isVirtualComposite = self._composites.shouldBeDisassembled(datasetType)
 
         # Add Registry Dataset entry.  If not a virtual composite, add
         # and attach components at the same time.
@@ -880,7 +895,7 @@ class Butler:
             filename = f"export.{format}"
         if directory is not None:
             filename = os.path.join(directory, filename)
-        BackendClass = getClassOf(self.config["repo_transfer_formats"][format]["export"])
+        BackendClass = getClassOf(self._config["repo_transfer_formats"][format]["export"])
         with open(filename, 'w') as stream:
             backend = BackendClass(stream)
             with self.transaction():
@@ -930,7 +945,7 @@ class Butler:
             filename = f"export.{format}"
         if directory is not None and not os.path.exists(filename):
             filename = os.path.join(directory, filename)
-        BackendClass = getClassOf(self.config["repo_transfer_formats"][format]["import"])
+        BackendClass = getClassOf(self._config["repo_transfer_formats"][format]["import"])
         with open(filename, 'r') as stream:
             backend = BackendClass(stream, self.registry)
             backend.register()
@@ -1017,7 +1032,7 @@ class Butler:
                     continue
 
                 # See if it is a StorageClass or a DatasetType
-                if key.name in self.storageClasses:
+                if key.name in self._storageClasses:
                     pass
                 else:
                     try:
