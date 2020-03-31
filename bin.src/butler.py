045344bf845fe28424ae4799d87a6ac5236a224f
Nathan Pease <npease@slac.stanford.edu>
2020-03-31 13:18:24 -0500

    change 'root' to 'REPO' per Jim's document

--- bin.src/butler.py
+++ bin.src/butler.py
@@ -13,23 +13,23 @@ def cli():
 
 
 @click.command(name='dump-config')
-@click.argument('root')
+@click.argument('repo')
 @click.option('--subset', '-s', type=str, help='Subset of a configuration to report. This can be any key in '
               "the hierarchy such as '.datastore.root' where the leading '.' specified the delimiter for "
               'the hierarchy.')
 @click.option('--searchpath', '-p', type=str, multiple=True,
               help='Additional search paths to use for configuration overrides')
 @click.option('--verbose', '-v', is_flag=True, help='Turn on debug reporting.')
-def dump_config(root, subset, searchpath, verbose):
+def dump_config(repo, subset, searchpath, verbose):
     '''Dump either a subset or full Butler configuration to standard output.
 
-    ROOT is the filesystem path for an existing Butler repository or path to
+    REPO is the filesystem path for an existing Butler repository or path to
     config file.
     '''
     if verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    config = ButlerConfig(root, searchPaths=searchpath)
+    config = ButlerConfig(repo, searchPaths=searchpath)
 
     if subset is not None:
         config = config[subset]
@@ -42,39 +42,39 @@ def dump_config(root, subset, searchpath, verbose):
 
 
 @click.command()
-@click.argument('root')
+@click.argument('repo')
 @click.option('--config', '-c', help='Path to an existing YAML config file to apply (on top of defaults).')
 @click.option('--standalone', is_flag=True, help='Include all defaults in the config file in the repo, '
               'insulating the repo from changes in package defaults.')
 @click.option('--override', '-o', is_flag=True, help='Allow values in the supplied config to override any '
-              'root settings.')
+              'repo settings.')
 @click.option('--outfile', '-f', default=None, type=str, help='Name of output file to receive repository '
-              'configuration. Default is to write butler.yaml into the specified root.')
+              'configuration. Default is to write butler.yaml into the specified repo.')
 @click.option('--verbose', '-v', is_flag=True, help='Turn on debug reporting.')
-def create(root, config, standalone, override, outfile, verbose):
+def create(repo, config, standalone, override, outfile, verbose):
     '''Create an empty Gen3 Butler repository.
 
-    ROOT is the filesystem path for the new repository. Will be created if it
+    REPO is the filesystem path for the new repository. Will be created if it
     does not exist.'''
     config = Config(config) if config is not None else None
-    Butler.makeRepo(root, config=config, standalone=standalone, forceConfigRoot=not override,
+    Butler.makeRepo(repo, config=config, standalone=standalone, forceConfigRoot=not override,
                     outfile=outfile)
 
 
 @click.command(name='validate-config')
-@click.argument('root')
+@click.argument('repo')
 @click.option('--quiet', '-q', is_flag=True, help="Do not report individual failures.")
 @click.option('--datasettype', '-d', type=str, multiple=True,
               help="Specific DatasetType(s) to validate (can be comma-separated)")
 @click.option('--ignore', '-i', type=str, multiple=True,
               help="DatasetType(s) to ignore for validation (can be comma-separated)")
-def validate_config(root, quiet, datasettype, ignore):
+def validate_config(repo, quiet, datasettype, ignore):
     '''Validate the configuration files for a Gen3 Butler repository.
 
-    ROOT is the filesystem path for an existing Butler repository.
+    REPO is the filesystem path for an existing Butler repository.
     '''
     logFailures = not quiet
-    butler = Butler(config=root)
+    butler = Butler(config=repo)
     try:
         butler.validateConfiguration(logFailures=logFailures, datasetTypeNames=datasettype, ignore=ignore)
     except ValidationError:
