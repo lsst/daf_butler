7fe815fddb8315b763200111cbc285a253317d05
Nathan Pease <npease@slac.stanford.edu>
2020-04-20 14:42:12 -0700

    add run_option

--- python/lsst/daf/butler/cli/opt/run.py
+++ python/lsst/daf/butler/cli/opt/run.py
@@ -1,8 +1,8 @@
-# This file is part of obs_base.
+# This file is part of daf_butler.
 #
 # Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
-# (https://www.lsst.org).
+# (http://www.lsst.org).
 # See the COPYRIGHT file at the top-level directory of this distribution
 # for details of code ownership.
 #
@@ -19,6 +19,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .dataset_type import dataset_type_option
-from .repo import repo_option
-from .run import run_option
+
+import click
+
+
+class run_option:  # noqa: N801
+    def __init__(self, required=False):
+        self.required = required
+
+    def __call__(self, f):
+        return click.option('--output-run',
+                            required=self.required,
+                            help='The name of the run datasets should be output to.')(f)
