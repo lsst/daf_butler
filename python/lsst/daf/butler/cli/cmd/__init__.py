0d6836a4d2b6b291f5785bf618d867ea5dbb9db9
Nathan Pease <npease@slac.stanford.edu>
2020-04-21 10:39:13 -0700

    replace validateButlerConfiguration with config-validate

--- python/lsst/daf/butler/cli/cmd/__init__.py
+++ python/lsst/daf/butler/cli/cmd/__init__.py
@@ -19,7 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ["create", "config_dump"]
+__all__ = ["create", "config_dump", "config_validate"]
 
 from .create import create
-from .dump_config import config_dump
+from .config_dump import config_dump
+from .config_validate import config_validate
