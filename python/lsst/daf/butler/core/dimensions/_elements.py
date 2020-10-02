4fe56e2e598dac7ac028c04693691f9db4762d0c
Jim Bosch <jbosch@astro.princeton.edu>
2020-10-02 00:01:52 -0400

    Overhaul dimension construction and class relationships.
    This is a three-pronged rewrite of a sizeable chunk of the dimensions
    codebase, inspired by DM-26336 prototyping:
    
     - There are now ABCs for DimensionElements (in _elements.py), with
       concrete implementations in _skypix.py and standard.py.  This
       provides a better separation of interface and implementation and
       sets the stage for more to come later.  We also now have an
       explicit interface (DimensionCombination) for DimensionElements
       that are not Dimensions, instead of just relying on the (previouly
       concrete) base class for that (though this doesn't do much yet).
       The interfaces of the DimensionElement classes are essentially
       unchanged by this, though many regular attributes have become
       properties.
    
     - DimensionUniverse construction now goes through a builder/visitor
       system (see the new construction.py module) that separates generic
       construction concerns from the config representation (useful for
       DM-26407, hope) and the post-construction state of the objects.  In
       many cases, construction is also simplified (or at least better
       organized) by deferring calculation of some attributes until first
       use, using the new cached_getter decorator to avoid recomputing
       them.
    
     - The system for describing the spatial and temporal attributes of
       DimensionElements has changed, with a number of new classes in
       _topology.py.  This removes the problematic (i.e. confusing and
       cyclical-dependency-inducing) fact that an element's spatial and
       temporal attributes were references to either self or some other
       element in order to model a priority order for related spatial
       elements (like tract and patch, or visit and
       visit_detector_region).  Instead, those attributes now point to new
       TopologicalFamily objects that explicitly hold their member
       priority order.  The new system will also play a role in allowing
       non-dimension spatial and temporal entities in queries in the near
       future (namely datasets in CALIBRATION collections, which are
       temporal via their validity ranges).
    
    Only the last of these is expected to affect code outside the
    dimensions package, and it's probable that even those won't affect
    anything outside daf_butler.  So while this is pretty big change
    (especially for one commit), it's also a highly localized one.

---
+++ python/lsst/daf/butler/core/dimensions/_elements.py
@@ -0,0 +1,297 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+__all__ = (
+    "Dimension",
+    "DimensionElement",
+)
+
+from abc import abstractmethod
+
+from typing import (
+    Any,
+    Optional,
+    Type,
+    TYPE_CHECKING,
+)
+
+from ..named import NamedValueAbstractSet, NamedValueSet
+from ..utils import cached_getter
+from .. import ddl
+
+from ._topology import TopologicalRelationshipEndpoint
+
+if TYPE_CHECKING:  # Imports needed only for type annotations; may be circular.
+    from ._universe import DimensionUniverse
+    from ._graph import DimensionGraph
+    from ._records import DimensionRecord
+
+
+class DimensionElement(TopologicalRelationshipEndpoint):
+    """A named data-organization concept that defines a label and/or metadata
+    in the dimensions system.
+
+    A `DimensionElement` instance typically corresponds to a _logical_ table in
+    the `Registry`: either an actual database table or a way of generating rows
+    on-the-fly that can similarly participate in queries.  The rows in that
+    table are represented by instances of a `DimensionRecord` subclass.  Most
+    `DimensionElement` instances are instances of its `Dimension` subclass,
+    which is used for elements that can be used as data ID keys.
+
+    Notes
+    -----
+    `DimensionElement` instances should always be constructed by and retreived
+    from a `DimensionUniverse`.  They are immutable after they are fully
+    constructed, and should never be copied.
+
+    Pickling a `DimensionElement` just records its name and universe;
+    unpickling one actually just looks up the element via the singleton
+    dictionary of all universes.  This allows pickle to be used to transfer
+    elements between processes, but only when each process initializes its own
+    instance of the same `DimensionUniverse`.
+    """
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.name})"
+
+    def __eq__(self, other: Any) -> bool:
+        try:
+            return self.name == other.name
+        except AttributeError:
+            # TODO: try removing this fallback; it's not really consistent with
+            # base class intent, and it could be confusing
+            return self.name == other
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    # TODO: try removing comparison operators; DimensionUniverse.sorted should
+    # be adequate.
+
+    def __lt__(self, other: DimensionElement) -> bool:
+        try:
+            return self.universe.getElementIndex(self.name) < self.universe.getElementIndex(other.name)
+        except KeyError:
+            return NotImplemented
+
+    def __le__(self, other: DimensionElement) -> bool:
+        try:
+            return self.universe.getElementIndex(self.name) <= self.universe.getElementIndex(other.name)
+        except KeyError:
+            return NotImplemented
+
+    def __gt__(self, other: DimensionElement) -> bool:
+        try:
+            return self.universe.getElementIndex(self.name) > self.universe.getElementIndex(other.name)
+        except KeyError:
+            return NotImplemented
+
+    def __ge__(self, other: DimensionElement) -> bool:
+        try:
+            return self.universe.getElementIndex(self.name) >= self.universe.getElementIndex(other.name)
+        except KeyError:
+            return NotImplemented
+
+    @classmethod
+    def _unpickle(cls, universe: DimensionUniverse, name: str) -> DimensionElement:
+        """Callable used for unpickling.
+
+        For internal use only.
+        """
+        return universe[name]
+
+    def __reduce__(self) -> tuple:
+        return (self._unpickle, (self.universe, self.name))
+
+    def __deepcopy__(self, memo: dict) -> DimensionElement:
+        # DimensionElement is recursively immutable; see note in @immutable
+        # decorator.
+        return self
+
+    def hasTable(self) -> bool:
+        """Return `True` if this element is associated with a table
+        (even if that table "belongs" to another element).
+        """
+        return True
+
+    universe: DimensionUniverse
+    """The universe of all compatible dimensions with which this element is
+    associated (`DimensionUniverse`).
+    """
+
+    name: str
+    """Unique name for this dimension element (`str`).
+    """
+
+    @property
+    @abstractmethod
+    def required(self) -> NamedValueAbstractSet[Dimension]:
+        """Dimensions that are sufficient (often necessary) to uniquely
+        identify a record of this dimension element.
+
+        For elements with a database representation, these dimension are
+        exactly those used to form the (possibly compound) primary key, and all
+        dimensions here that are not ``self`` are also used to form foreign
+        keys.
+
+        For `Dimension` instances, this should be exactly the same as
+        ``graph.required``, but that may not be true for `DimensionElement`
+        instances in general.  When they differ, there are multiple
+        combinations of dimensions that uniquely identify this element, but
+        this one is more direct.
+        """
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def implied(self) -> NamedValueAbstractSet[Dimension]:
+        """Other dimensions that are uniquely identified directly by a record
+        of this dimension element.
+
+        For elements with a database representation, these are exactly the
+        dimensions used to form foreign key constraints whose fields are not
+        (wholly) also part of the primary key.
+
+        Unlike ``self.graph.implied``, this set is not expanded recursively.
+        """
+        raise NotImplementedError()
+
+    @property  # type: ignore
+    @cached_getter
+    def dimensions(self) -> NamedValueAbstractSet[Dimension]:
+        """The union of `required` and `implied`, with all elements in
+        `required` before any elements in `implied`.
+
+        This differs from ``self.graph.dimensions`` both in order and in
+        content:
+
+        - as in ``self.implied``, implied dimensions are not expanded
+          recursively here;
+        - implied dimensions appear after required dimensions here, instead of
+          being topologically ordered.
+
+        As a result, this set is ordered consistently with
+        ``self.RecordClass.fields``.
+        """
+        return NamedValueSet(list(self.required) + list(self.implied)).freeze()
+
+    @property  # type: ignore
+    @cached_getter
+    def graph(self) -> DimensionGraph:
+        """Minimal graph that includes this element (`DimensionGraph`).
+
+        ``self.graph.required`` includes all dimensions whose primary key
+        values are sufficient (often necessary) to uniquely identify ``self``
+        (including ``self` if ``isinstance(self, Dimension)``.
+        ``self.graph.implied`` includes all dimensions also identified
+        (possibly recursively) by this set.
+        """
+        return self.universe.extract(self.dimensions.names)
+
+    @property  # type: ignore
+    @cached_getter
+    def RecordClass(self) -> Type[DimensionRecord]:
+        """The `DimensionRecord` subclass used to hold records for this element
+        (`type`).
+
+        Because `DimensionRecord` subclasses are generated dynamically, this
+        type cannot be imported directly and hence can only be obtained from
+        this attribute.
+        """
+        from ._records import _subclassDimensionRecord
+        return _subclassDimensionRecord(self)
+
+    @property
+    @abstractmethod
+    def metadata(self) -> NamedValueAbstractSet[ddl.FieldSpec]:
+        """Additional metadata fields included in this element's table
+        (`NamedValueSet` of `FieldSpec`).
+        """
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def cached(self) -> bool:
+        """Whether `Registry` should cache records of this element in-memory
+        (`bool`).
+        """
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def viewOf(self) -> Optional[str]:
+        """Name of another table this element's records are drawn from (`str`
+        or `None`).
+        """
+        raise NotImplementedError()
+
+    @property
+    def alwaysJoin(self) -> bool:
+        # TODO: docs
+        return False
+
+
+class Dimension(DimensionElement):
+    """A named data-organization concept that can be used as a key in a data
+    ID.
+    """
+
+    @property
+    @abstractmethod
+    def uniqueKeys(self) -> NamedValueAbstractSet[ddl.FieldSpec]:
+        """All fields that can individually be used to identify records of this
+        element, given the primary keys of all required dependencies
+        (`NamedValueAbstractSet` of `FieldSpec`).
+        """
+        raise NotImplementedError()
+
+    @property  # type: ignore
+    @cached_getter
+    def primaryKey(self) -> ddl.FieldSpec:
+        """The primary key field for this dimension (`FieldSpec`).
+
+        Note that the database primary keys for dimension tables are in general
+        compound; this field is the only field in the database primary key that
+        is not also a foreign key (to a required dependency dimension table).
+        """
+        primaryKey, *_ = self.uniqueKeys
+        return primaryKey
+
+    @property  # type: ignore
+    @cached_getter
+    def alternateKeys(self) -> NamedValueAbstractSet[ddl.FieldSpec]:
+        """Additional unique key fields for this dimension that are not the the
+        primary key (`NamedValueAbstractSet` of `FieldSpec`).
+        """
+        _, *alternateKeys = self.uniqueKeys
+        return NamedValueSet(alternateKeys).freeze()
+
+
+class DimensionCombination(DimensionElement):
+    """A `DimensionElement` that provides extra metadata and/or relationship
+    endpoint information for a combination of dimensions.
+    """
+    pass
---
+++ python/lsst/daf/butler/core/dimensions/construction.py
@@ -0,0 +1,198 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import (
+    AbstractSet,
+    Dict,
+    Iterable,
+    TYPE_CHECKING,
+)
+
+from ..named import NamedValueSet
+from ._topology import TopologicalFamily, TopologicalSpace
+
+if TYPE_CHECKING:
+    from ._elements import Dimension, DimensionElement
+    from ._packer import DimensionPackerFactory
+
+
+class DimensionConstructionVisitor(ABC):
+    """An abstract base class for adding one or more entities to a
+    `DimensionConstructionBuilder`.
+
+    Parameters
+    ----------
+    name : `str`
+        Name of an entity being added.  This must be unique across all
+        entities, which include `DimensionElement`, `TopologicalFamily`, and
+        `DimensionPackerFactory` objects.  The visitor may add other entities
+        as well, as long as only the named entity is referenced by other
+        entities in the universe.
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    @abstractmethod
+    def hasDependenciesIn(self, others: AbstractSet[str]) -> bool:
+        """Test whether other entities this visitor depends on have already
+        been constructed.
+
+        Parameters
+        ----------
+        others : `AbstractSet` [ `str` ]
+            The names of other visitors that have not yet been invoked.
+
+        Returns
+        -------
+        blocked : `bool`
+            If `True`, this visitor has dependencies on other visitors that
+            must be invoked before this one can be.  If `False`, `visit` may
+            be called.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def visit(self, builder: DimensionConstructionBuilder) -> None:
+        """Modify the given builder object to include the entities this object
+        is responsible for constructing.
+
+        Parameters
+        ----------
+        builder : `DimensionConstructionBuilder`
+            Builder to modify in-place and from which dependencies can be
+            obtained.
+        """
+        raise NotImplementedError()
+
+
+class DimensionConstructionBuilder:
+    """A builder object for constructing `DimensionUniverse` instances.
+
+    `DimensionConstructionVisitor` objects can be added to a
+    `DimensionConstructionBuilder` object in any order, and are invoked
+    in a deterministic order consistent with their dependency relationships
+    by a single call (by the `DimensionUnvierse`) to the `finish` method.
+
+    Parameters
+    ----------
+    version : `int`
+        Version for the `DimensionUniverse`.
+    commonSkyPixName : `str`
+        Name of the "common" skypix dimension that is used to relate all other
+        spatial `TopologicalRelationshipEndpoint` objects.
+    visitors : `Iterable` [ `DimensionConstructionVisitor` ]
+        Visitor instances to include from the start.
+    """
+
+    def __init__(
+        self,
+        version: int,
+        commonSkyPixName: str, *,
+        visitors: Iterable[DimensionConstructionVisitor] = ()
+    ) -> None:
+        self.dimensions = NamedValueSet()
+        self.elements = NamedValueSet()
+        self.topology = {space: NamedValueSet() for space in TopologicalSpace.__members__.values()}
+        self.packers = {}
+        self.version = version
+        self.commonSkyPixName = commonSkyPixName
+        self._todo: Dict[str, DimensionConstructionVisitor] = {v.name: v for v in visitors}
+
+    def add(self, visitor: DimensionConstructionVisitor) -> None:
+        """Add a single visitor to the builder.
+
+        Parameters
+        ----------
+        visitor : `DimensionConstructionVisitor`
+            Visitor instance to add.
+        """
+        self._todo[visitor.name] = visitor
+
+    def update(self, visitors: Iterable[DimensionConstructionVisitor]) -> None:
+        """Add multiple visitors to the builder.
+
+        Parameters
+        ----------
+        visitors : `Iterable` [ `DimensionConstructionVisitor` ]
+            Visitor instances to add.
+        """
+        self._todo.update((v.name, v) for v in visitors)
+
+    def finish(self) -> None:
+        """Complete construction of the builder.
+
+        This method invokes all visitors in an order consistent with their
+        dependencies, fully populating all public attributes.  It should be
+        called only once, by `DimensionUniverse` itself.
+        """
+        while self._todo:
+            unblocked = [name for name, visitor in self._todo.items()
+                         if not visitor.hasDependenciesIn(self._todo.keys())]
+            unblocked.sort()  # Break ties lexicographically.
+            if not unblocked:
+                raise RuntimeError(f"Cycle or unmet dependency in dimension elements: {self._todo.keys()}.")
+            for name in unblocked:
+                self._todo.pop(name).visit(self)
+
+    version: int
+    """Version number for the `DimensionUniverse` (`int`).
+
+    Populated at builder construction.
+    """
+
+    commonSkyPixName: str
+    """Name of the common skypix dimension used to connect other spatial
+    `TopologicalRelationshipEndpoint` objects (`str`).
+
+    Populated at builder construction.
+    """
+
+    dimensions: NamedValueSet[Dimension]
+    """Set of all `Dimension` objects (`NamedValueSet` [ `Dimension` ]).
+
+    Populated by `finish`.
+    """
+
+    elements: NamedValueSet[DimensionElement]
+    """Set of all `DimensionElement` objects
+    (`NamedValueSet` [ `DimensionElement` ]).
+
+    Populated by `finish`.  `DimensionConstructionVisitor` classes that
+    construct `Dimension` objects are responsible for adding them to this
+    set as well as `dimensions`.
+    """
+
+    topology: Dict[TopologicalSpace, NamedValueSet[TopologicalFamily]]
+    """Dictionary containing all `TopologicalFamily` objects
+    (`dict` [ `TopologicalSpace`, `NamedValueSet` [ `TopologicalFamily` ] ] ).
+    """
+
+    packers: Dict[str, DimensionPackerFactory]
+    """Dictionary containing all `DimensionPackerFactory` objects
+    (`dict` [ `str`, `DimensionPackerFactory` ] ).
+    """
