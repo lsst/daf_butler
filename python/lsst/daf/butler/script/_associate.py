lsst/butler_prototype

add butler associate command

python/lsst/daf/butler/script/_associate.py
@@ -1,0 +1,45 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from .. import Butler, CollectionType
+from ..script import QueryDatasets
+
+
+def associate(repo, collection, dataset_type, collections, where, find_first):
+    """Add existing datasets to ta CHAINED collection.
+    """
+
+    butler = Butler(repo, writeable=True)
+
+    if butler.registry.getCollectionType(collection) is not CollectionType.CHAINED:
+        raise RuntimeError("Can only add datasets to a chained collection.")
+
+    results = QueryDatasets(
+        butler=butler,
+        glob=dataset_type,
+        collections=collections,
+        where=where,
+        find_first=find_first,
+        show_uri=False,
+        repo=None
+    )
+
+    butler.registry.associate(collection, results.getDatasets())

lsst/daf_butler

python/lsst/daf/butler/script/_associate.py
@@ -1,0 +1,44 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from .. import Butler, CollectionType
+from ..script import QueryDatasets
+
+
+def associate(repo, collection, dataset_type, collections, where, find_first):
+    """Add existing datasets to a CHAINED collection.
+    """
+
+    butler = Butler(repo, writeable=True)
+
+    butler.registry.registerCollection(collection, CollectionType.TAGGED)
+
+    results = QueryDatasets(
+        butler=butler,
+        glob=dataset_type,
+        collections=collections,
+        where=where,
+        find_first=find_first,
+        show_uri=False,
+        repo=None
+    )
+
+    butler.registry.associate(collection, results.getDatasets())