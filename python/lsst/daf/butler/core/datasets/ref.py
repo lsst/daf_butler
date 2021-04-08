62921767b24d7b46ff9729d3879764953502e7cf
Tim Jenness <tjenness@lsst.org>
2021-04-08 16:09:50 -0700

    Fix naming flake8 violation from the pydantic @validator

--- python/lsst/daf/butler/core/datasets/ref.py
+++ python/lsst/daf/butler/core/datasets/ref.py
@@ -68,19 +68,19 @@ class SerializedDatasetRef(BaseModel):
     component: Optional[StrictStr] = None
 
     @validator("dataId")
-    def check_dataId(cls, v: Any, values: Dict[str, Any]) -> Any:
+    def check_dataId(cls, v: Any, values: Dict[str, Any]) -> Any:  # noqa: N805
         if (d := "datasetType") in values and values[d] is None:
             raise ValueError("Can not specify 'dataId' without specifying 'datasetType'")
         return v
 
     @validator("run")
-    def check_run(cls, v: Any, values: Dict[str, Any]) -> Any:
+    def check_run(cls, v: Any, values: Dict[str, Any]) -> Any:  # noqa: N805
         if v and (i := "id") in values and values[i] is None:
             raise ValueError("'run' cannot be provided unless 'id' is.")
         return v
 
     @validator("component")
-    def check_component(cls, v: Any, values: Dict[str, Any]) -> Any:
+    def check_component(cls, v: Any, values: Dict[str, Any]) -> Any:  # noqa: N805
         # Component should not be given if datasetType is given
         if v and (d := "datasetType") in values and values[d] is not None:
             raise ValueError(f"datasetType ({values[d]}) can not be set if component is given ({v}).")
