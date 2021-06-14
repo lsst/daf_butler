c43b602ba851936a9a9b4c57e93da1ec99f3c9b7
Tim Jenness <tjenness@lsst.org>
2021-06-14 12:56:44 -0700

    Defer creation of temporary datastore cache directory
    Previously we always created it every single time someone instantiated
    a Butler!

--- python/lsst/daf/butler/core/datastoreCacheManager.py
+++ python/lsst/daf/butler/core/datastoreCacheManager.py
@@ -156,14 +156,21 @@ class DatastoreCacheManager(AbstractDatastoreCacheManager):
                  universe: DimensionUniverse):
         super().__init__(config, universe)
 
-        if (root := self.config.get("root")):
-            self.cache_directory = ButlerURI(root, forceAbsolute=True)
-        else:
-            self.cache_directory = ButlerURI(tempfile.mkdtemp(prefix="butler-"), forceDirectory=True,
-                                             isTemporary=True)
+        # Set cache directory if it pre-exists, else defer creation until
+        # requested.
+        root = self.config.get("root")
+        self._cache_directory = ButlerURI(root, forceAbsolute=True) if root is not None else None
 
         # Calculate the caching lookup table.
         self._lut = processLookupConfigs(self.config["cacheable"], universe=universe)
+
+    @property
+    def cache_directory(self) -> ButlerURI:
+        if self._cache_directory is None:
+            # Create on demand.
+            self._cache_directory = ButlerURI(tempfile.mkdtemp(prefix="butler-"), forceDirectory=True,
+                                              isTemporary=True)
+        return self._cache_directory
 
     def should_be_cached(self, entity: Union[DatasetRef, DatasetType, StorageClass]) -> bool:
         # Docstring inherited
