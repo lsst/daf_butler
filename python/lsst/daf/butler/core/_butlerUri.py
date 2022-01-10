ac16e0e0c6999e3587d0209f40befb6035dceffd
Tim Jenness <tjenness@lsst.org>
2022-01-10 14:39:31 -0700

    Create new ButlerURI class with deprecation message
    This implementation does not currently support people
    using `isinstance(uri, ButlerURI)` since it always
    returns a ResourcePath.

--- python/lsst/daf/butler/core/_butlerUri.py
+++ python/lsst/daf/butler/core/_butlerUri.py
@@ -1,2 +1,20 @@
 # Temporarily alias ResourcePath as ButlerURI.
-from lsst.resources import ResourcePath as ButlerURI  # noqa: F401
+from typing import Any
+
+from deprecated.sphinx import deprecated
+from lsst.resources import ResourcePath, ResourcePathExpression
+
+
+@deprecated(
+    reason="Please use lsst.resources.ResourcePath instead. Will be removed after v24.",
+    version="v24.0",
+    category=FutureWarning,
+)
+class ButlerURI(ResourcePath):
+    """Class for handling URIs to file resources.
+
+    All users should instead use `lsst.resources.ResourcePath`.
+    """
+
+    def __new__(cls, uri: ResourcePathExpression, **kwargs: Any) -> ResourcePath:  # type: ignore
+        return ResourcePath(uri, **kwargs)
