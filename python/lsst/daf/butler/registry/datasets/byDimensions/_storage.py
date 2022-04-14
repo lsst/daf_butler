7c8d096a921e1e9a828200999f82a5b346fb8faa
Jim Bosch <jbosch@astro.princeton.edu>
2022-04-14 14:38:07 -0400

    Add option to compute collection rank in dataset subqueries.
    This provides an alternate implementation for constructing an important
    aspect of find-first dataset queries, but it isn't actually used yet.
    
    This case-based approach is much closer to Andy Salnikov's initial
    implementation of find-first dataset queries; I think it's likely that
    changing it was at most unnecessary performance-wise (I was looking for
    ways to simplify a query the planner was having trouble with, but this
    construct - despite being hard for humans to parse - probably wasn't
    playing a role in that).  And some other query system changes have made
    it preferable from a code-organization standpoint, providing its no
    worse from a performance standpoint (ideally, there would be no
    difference, as the query planner would recognize the approaches as
    identical, but I don't want to assume that).

--- python/lsst/daf/butler/registry/datasets/byDimensions/_storage.py
+++ python/lsst/daf/butler/registry/datasets/byDimensions/_storage.py
@@ -323,6 +323,7 @@ class ByDimensionsDatasetRecordStorage(DatasetRecordStorage):
         run: SimpleQuery.Select.Or[None] = SimpleQuery.Select,
         timespan: SimpleQuery.Select.Or[Optional[Timespan]] = SimpleQuery.Select,
         ingestDate: SimpleQuery.Select.Or[Optional[Timespan]] = None,
+        rank: SimpleQuery.Select.Or[None] = None,
     ) -> sqlalchemy.sql.Selectable:
         # Docstring inherited from DatasetRecordStorage.
         collection_types = {collection.type for collection in collections}
@@ -384,7 +385,13 @@ class ByDimensionsDatasetRecordStorage(DatasetRecordStorage):
                     TimespanReprClass.fromLiteral(None).overlaps(TimespanReprClass.fromLiteral(timespan))
                 )
             self._finish_single_select(
-                tags_query, self._tags, collections, id=id, run=run, ingestDate=ingestDate
+                tags_query,
+                self._tags,
+                collections,
+                id=id,
+                run=run,
+                ingestDate=ingestDate,
+                rank=rank,
             )
         else:
             tags_query = None
@@ -408,7 +415,13 @@ class ByDimensionsDatasetRecordStorage(DatasetRecordStorage):
                     )
                 )
             self._finish_single_select(
-                calibs_query, self._calibs, collections, id=id, run=run, ingestDate=ingestDate
+                calibs_query,
+                self._calibs,
+                collections,
+                id=id,
+                run=run,
+                ingestDate=ingestDate,
+                rank=rank,
             )
         else:
             calibs_query = None
@@ -429,6 +442,7 @@ class ByDimensionsDatasetRecordStorage(DatasetRecordStorage):
         id: SimpleQuery.Select.Or[Optional[int]],
         run: SimpleQuery.Select.Or[None],
         ingestDate: SimpleQuery.Select.Or[Optional[Timespan]],
+        rank: SimpleQuery.Select.Or[None],
     ) -> None:
         dataset_id_col = table.columns.dataset_id
         collection_col = table.columns[self._collections.getCollectionForeignKeyName()]
@@ -446,6 +460,16 @@ class ByDimensionsDatasetRecordStorage(DatasetRecordStorage):
             query.where.append(sqlalchemy.sql.literal(False))
         else:
             query.where.append(collection_col.in_([collection.key for collection in collections]))
+        # Add rank if requested as a CASE-based calculation the collection
+        # column.
+        if rank is not None:
+            assert rank is SimpleQuery.Select, "Cannot constraint rank, only select it."
+            query.columns.append(
+                sqlalchemy.sql.case(
+                    {record.key: n for n, record in enumerate(collections)},
+                    value=collection_col,
+                ).label("rank")
+            )
         # We can always get the dataset_id from the tags/calibs table or
         # constrain it there.  Can't use kwargs for that because we need to
         # alias it to 'id'.
