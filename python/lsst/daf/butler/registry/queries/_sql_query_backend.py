6d8904b713c82318a3a4c9c28931fd5a9179e934
Jim Bosch <jbosch@astro.princeton.edu>
2022-07-22 15:09:20 -0400

    Add QueryBackend stub classes.
    These don't do anything yet; we'll be adding methods incrementally to
    them in future commits to encapsulate query construction logic behind
    an interface that both SqlRegistry and RemoteRegistry can support.

---
+++ python/lsst/daf/butler/registry/queries/_sql_query_backend.py
@@ -0,0 +1,57 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import annotations
+
+__all__ = ("SqlQueryBackend",)
+
+from typing import TYPE_CHECKING
+
+from ._query_backend import QueryBackend
+
+if TYPE_CHECKING:
+    from ...core import DimensionUniverse
+    from ..interfaces import Database
+    from ..managers import RegistryManagerInstances
+
+
+class SqlQueryBackend(QueryBackend):
+    """An implementation of `QueryBackend` for `SqlRegistry`.
+
+    Parameters
+    ----------
+    db : `Database`
+        Object that abstracts the database engine.
+    managers : `RegistryManagerInstances`
+        Struct containing the manager objects that back a `SqlRegistry`.
+    """
+
+    def __init__(
+        self,
+        db: Database,
+        managers: RegistryManagerInstances,
+    ):
+        self._db = db
+        self._managers = managers
+
+    @property
+    def universe(self) -> DimensionUniverse:
+        # Docstring inherited.
+        return self._managers.dimensions.universe
