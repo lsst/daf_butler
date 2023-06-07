3d6cecfc3cfb2710f0d738a9d7e42c20d5e08eb4
Tim Jenness <tjenness@lsst.org>
2023-06-07 15:15:16 -0700

    Rewrite ButlerRepoIndex failure reporting
    Now cache the actual exception for later retrieval and
    reset it on each call.

--- python/lsst/daf/butler/_butlerRepoIndex.py
+++ python/lsst/daf/butler/_butlerRepoIndex.py
@@ -26,7 +26,7 @@ __all__ = ("ButlerRepoIndex",)
 import os
 from typing import ClassVar, Dict, Set
 
-from lsst.resources import ResourcePath, ResourcePathExpression
+from lsst.resources import ResourcePath
 
 from .core import Config
 
@@ -55,13 +55,17 @@ class ButlerRepoIndex:
     """Cache of indexes. In most scenarios only one index will be found
     and the environment will not change. In tests this may not be true."""
 
+    _most_recent_failure: ClassVar[str] = ""
+    """Cache of the most recent failure when reading an index. Reset on
+    every read."""
+
     @classmethod
-    def _read_repository_index(cls, index_uri: ResourcePathExpression) -> Config:
+    def _read_repository_index(cls, index_uri: ResourcePath) -> Config:
         """Read the repository index from the supplied URI.
 
         Parameters
         ----------
-        index_uri : `lsst.resources.ResourcePathExpression`
+        index_uri : `lsst.resources.ResourcePath`
             URI of the repository index.
 
         Returns
@@ -78,23 +82,19 @@ class ButlerRepoIndex:
         -----
         Does check the cache before reading the file.
         """
-        # Force the given value to a ResourcePath so that it can be used
-        # as an index into the cache consistently.
-        uri = ResourcePath(index_uri)
-
         if index_uri in cls._cache:
-            return cls._cache[uri]
+            return cls._cache[index_uri]
 
         try:
-            repo_index = Config(uri)
+            repo_index = Config(index_uri)
         except FileNotFoundError as e:
             # More explicit error message.
-            raise FileNotFoundError(f"Butler repository index file not found at {uri}.") from e
+            raise FileNotFoundError(f"Butler repository index file not found at {index_uri}.") from e
         except Exception as e:
             raise RuntimeError(
-                f"Butler repository index file at {uri} could not be read: {type(e).__qualname__} {e}"
+                f"Butler repository index file at {index_uri} could not be read: {type(e).__qualname__} {e}"
             ) from e
-        cls._cache[uri] = repo_index
+        cls._cache[index_uri] = repo_index
 
         return repo_index
 
@@ -126,8 +126,18 @@ class ButlerRepoIndex:
         repo_index : `Config`
             The index found in the environment.
         """
-        index_uri = cls._get_index_uri()
-        return cls._read_repository_index(index_uri)
+        cls._most_recent_failure = ""
+        try:
+            index_uri = cls._get_index_uri()
+        except KeyError as e:
+            cls._most_recent_failure = str(e)
+            raise
+        try:
+            repo_index = cls._read_repository_index(index_uri)
+        except Exception as e:
+            cls._most_recent_failure = str(e)
+            raise
+        return repo_index
 
     @classmethod
     def get_known_repos(cls) -> Set[str]:
@@ -154,12 +164,14 @@ class ButlerRepoIndex:
             If there is a problem reading the repository index, this will
             contain a string with an explanation. Empty string if everything
             worked.
+
+        Notes
+        -----
+        The value returned is only reliable if called immediately after a
+        failure. The most recent failure reason is reset every time an attempt
+        is made to request a label and so the reason can be out of date.
         """
-        try:
-            cls._read_repository_index_from_environment()
-        except Exception as e:
-            return str(e)
-        return ""
+        return cls._most_recent_failure
 
     @classmethod
     def get_repo_uri(cls, label: str, return_label: bool = False) -> ResourcePath:
