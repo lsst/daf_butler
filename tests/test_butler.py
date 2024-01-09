bfe79a33deaab6afb96672df945f78b4cbe10623
David H. Irving <david.irving@noirlab.edu>
2024-01-08 17:08:31 -0700

    Make Datastore cloneable
    To facilitate this, altered the Datastore interface to decouple the constructor from Datastore.fromConfig().  Datastores now have to implement a clone() method and _create_from_config() that can be called by Datastore.fromConfig().

--- tests/test_butler.py
+++ tests/test_butler.py
@@ -604,7 +604,8 @@ class ButlerTests(ButlerPutGetTests):
         butler2 = Butler.from_config(butler=butler, collections=["other"])
         self.assertEqual(butler2.collections, ("other",))
         self.assertIsNone(butler2.run)
-        self.assertIs(butler._datastore, butler2._datastore)
+        self.assertEqual(type(butler._datastore), type(butler2._datastore))
+        self.assertEqual(butler._datastore.config, butler2._datastore.config)
 
         # Test that we can use an environment variable to find this
         # repository.
