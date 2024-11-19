f5fbab5c5bcbb5200d7f3cc9822ccd4bb2510b64
David H. Irving <david.irving@noirlab.edu>
2024-11-19 13:27:43 -0700

    Add manual spans for streaming queries
    Add manual spans to clarify where time is being used during streaming queries

--- python/lsst/daf/butler/remote_butler/server/_telemetry.py
+++ python/lsst/daf/butler/remote_butler/server/_telemetry.py
@@ -25,22 +25,54 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Any
+from collections.abc import Iterator
+from contextlib import AbstractContextManager, contextmanager
+from typing import Any, Protocol
+
+try:
+    import sentry_sdk
+except ImportError:
+    sentry_sdk = None
+
+
+class TelemetryContext(Protocol):
+    def span(self, name: str) -> AbstractContextManager[None]: ...
+
+
+class NullTelemetryContext(TelemetryContext):
+    @contextmanager
+    def span(self, name: str) -> Iterator[None]:
+        yield
+
+
+class SentryTelemetryContext(TelemetryContext):
+    @contextmanager
+    def span(self, name: str) -> Iterator[None]:
+        with sentry_sdk.start_span(name=name):
+            yield
+
+
+_telemetry_context: TelemetryContext = NullTelemetryContext()
 
 
 def enable_telemetry() -> None:
     """Turn on upload of trace telemetry to Sentry, to allow performance
     debugging of deployed server.
     """
-    try:
-        import sentry_sdk
-    except ImportError:
+    if sentry_sdk is None:
         return
 
     # Configuration will be pulled from SENTRY_* environment variables
     # (see https://docs.sentry.io/platforms/python/configuration/options/).
     # If SENTRY_DSN is not present, telemetry is disabled.
     sentry_sdk.init(enable_tracing=True, traces_sampler=_decide_whether_to_sample_trace)
+
+    global _telemetry_context
+    _telemetry_context = SentryTelemetryContext()
+
+
+def get_telemetry_context() -> TelemetryContext:
+    return _telemetry_context
 
 
 def _decide_whether_to_sample_trace(context: dict[str, Any]) -> float:
