20860d2b29d2f3d3680e84e0abe7d906cd519741
David H. Irving <david.irving@noirlab.edu>
2024-12-06 11:10:27 -0700

    Revert "DO NOT MERGE: enable sentry profiling"
    This reverts commit 4a95b721a2d6cfe635408e1cddff04b7a425302b.

--- python/lsst/daf/butler/remote_butler/server/_telemetry.py
+++ python/lsst/daf/butler/remote_butler/server/_telemetry.py
@@ -65,9 +65,7 @@ def enable_telemetry() -> None:
     # Configuration will be pulled from SENTRY_* environment variables
     # (see https://docs.sentry.io/platforms/python/configuration/options/).
     # If SENTRY_DSN is not present, telemetry is disabled.
-    sentry_sdk.init(
-        enable_tracing=True, traces_sampler=_decide_whether_to_sample_trace, profiles_sample_rate=1.0
-    )
+    sentry_sdk.init(enable_tracing=True, traces_sampler=_decide_whether_to_sample_trace)
 
     global _telemetry_context
     _telemetry_context = SentryTelemetryContext()
