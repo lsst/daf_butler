f45a86ab6997fd8f4484c321c8d7b7b8ca403a77
David H. Irving <david.irving@noirlab.edu>
2026-01-09 14:14:20 -0700

    Relax the signature for LabeledButlerFactory.create_butler
    The label is always required and in many use cases the access token is not, so allow the label to be positional and make the access token optional.

--- tests/test_butler_factory.py
+++ tests/test_butler_factory.py
@@ -67,7 +67,7 @@ class ButlerFactoryTestCase(unittest.TestCase):
                 self.assertIsNotNone(butler.collections.get_info("new_run"))
 
     def _test_factory(self, factory: LabeledButlerFactory) -> None:
-        butler = factory.create_butler(label="test_repo", access_token=None)
+        butler = factory.create_butler("test_repo")
         self.assertIsInstance(butler, DirectButler)
         # This identical second call covers a read from the cache.
         butler2 = factory.create_butler(label="test_repo", access_token=None)
