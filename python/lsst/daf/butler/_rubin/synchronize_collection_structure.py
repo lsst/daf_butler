c52a0e563a8459776835cef1fef252b74ab81cad
David H. Irving <david.irving@noirlab.edu>
2026-02-10 14:20:59 -0700

    Add function to synchronize collection chain structure

---
+++ python/lsst/daf/butler/_rubin/synchronize_collection_structure.py
@@ -0,0 +1,34 @@
+from collections.abc import Iterable
+
+from .._butler import Butler
+from .._butler_collections import CollectionInfo
+from .._collection_type import CollectionType
+
+
+def synchronize_collection_structure(
+    source_butler: Butler, target_butler: Butler, collections: Iterable[str]
+) -> None:
+    source_info = source_butler.collections.query_info(
+        collections, include_doc=True, include_chains=True, flatten_chains=True
+    )
+    target_info = target_butler.collections.query_info([c.name for c in source_info], ignore_missing=True)
+    target_map = {c.name: c for c in target_info}
+
+    missing_collections: list[CollectionInfo] = []
+    mismatched_children: dict[str, tuple[str, ...]] = {}
+    for source_collection in source_info:
+        target_collection = target_map.get(source_collection.name)
+        if not target_collection:
+            missing_collections.append(source_collection)
+            if source_collection.type == CollectionType.CHAINED:
+                mismatched_children[source_collection.name] = source_collection.children
+        elif source_collection.children != target_collection.children:
+            mismatched_children[source_collection.name] = source_collection.children
+
+    for collection in missing_collections:
+        print(f"register {collection.name}")
+        target_butler.collections.register(collection.name, type=collection.type, doc=collection.doc)
+
+    for parent, children in mismatched_children.items():
+        print(f"define collection chain {parent}: {children}")
+        target_butler.collections.redefine_chain(parent, children)
