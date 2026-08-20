12286cd56c638740b782bd79f89ccbda1fa0e34e
David H. Irving <david.irving@noirlab.edu>
2026-08-19 17:39:21 -0700

    Remove defunct requirements files
    These are now superseded by uv.lock.

--- requirements/upgrade-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pip install uv
-
-REQUIREMENTS_DIR=$(dirname "$0")
-uv pip compile --upgrade --generate-hashes --build-isolation --output-file $REQUIREMENTS_DIR/docker.txt $REQUIREMENTS_DIR/docker.in
-uv pip compile --upgrade --generate-hashes --build-isolation --output-file $REQUIREMENTS_DIR/docker-test.txt $REQUIREMENTS_DIR/docker-test.in
